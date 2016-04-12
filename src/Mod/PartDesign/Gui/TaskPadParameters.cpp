FreeCAD/FreeCAD

Pad/Pocket: Allow extrude up to a datum plane

src/Mod/PartDesign/Gui/TaskPadParameters.cpp
@@ -99,6 +99,7 @@ TaskPadParameters::TaskPadParameters(ViewProviderPad *PadView,QWidget *parent)
     bool reversed = pcPad->Reversed.getValue();
     double l2 = pcPad->Length2.getValue();
     int index = pcPad->Type.getValue(); // must extract value here, clear() kills it!
+    App::DocumentObject* obj = pcPad->UpToFace.getValue();
     std::vector<std::string> subStrings = pcPad->UpToFace.getSubValues();
     std::string upToFace;
     int faceId = -1;
@@ -119,9 +120,13 @@ TaskPadParameters::TaskPadParameters(ViewProviderPad *PadView,QWidget *parent)
     // According to bug #0000521 the reversed option
     // shouldn't be de-activated if the pad has a support face
     ui->checkBoxReversed->setChecked(reversed);
-    ui->lineFaceName->setText(faceId >= 0 ?
-                              tr("Face") + QString::number(faceId) :
-                              tr("No face selected"));
+    if (PartDesign::Feature::isDatum(obj))
+        ui->lineFaceName->setText(QString::fromAscii(obj->getNameInDocument()));
+    else if (faceId >= 0)
+        ui->lineFaceName->setText(QString::fromAscii(obj->getNameInDocument()) + tr("Face") +
+                                  QString::number(faceId));
+    else
+        ui->lineFaceName->setText(tr("No face selected"));
     ui->lineFaceName->setProperty("FaceName", QByteArray(upToFace.c_str()));
     ui->changeMode->clear();
     ui->changeMode->insertItem(0, tr("Dimension"));
@@ -175,7 +180,7 @@ void TaskPadParameters::updateUI(int index)
         ui->lineFaceName->setEnabled(true);
         QMetaObject::invokeMethod(ui->lineFaceName, "setFocus", Qt::QueuedConnection);
         // Go into reference selection mode if no face has been selected yet
-        if (ui->lineFaceName->text().isEmpty())
+        if (ui->lineFaceName->text().isEmpty() || (ui->lineFaceName->text() == tr("No face selected")))
             onButtonFace(true);
     } else { // two dimensions
         ui->doubleSpinBox->setEnabled(true);
@@ -310,9 +315,12 @@ int TaskPadParameters::getMode(void) const
     return ui->changeMode->currentIndex();
 }
 
-QByteArray TaskPadParameters::getFaceName(void) const
+const std::string TaskPadParameters::getFaceName(void) const
 {
-    return ui->lineFaceName->property("FaceName").toByteArray();
+    if ((getMode() >= 1) || (getMode() <= 3))
+        return getFaceReference(ui->lineFaceName->text(), ui->lineFaceName->property("FaceName").toString()).toStdString();
+    else
+        return "";
 }
 
 const bool TaskPadParameters::updateView() const
@@ -343,14 +351,15 @@ void TaskPadParameters::changeEvent(QEvent *e)
         ui->changeMode->addItem(tr("Two dimensions"));
         ui->changeMode->setCurrentIndex(index);
 
-        QByteArray upToFace = this->getFaceName();
+        QStringList parts = ui->lineFaceName->text().split(QChar::fromAscii(':'));
+        QByteArray upToFace = ui->lineFaceName->property("FaceName").toByteArray();
         int faceId = -1;
         bool ok = false;
         if (upToFace.indexOf("Face") == 0) {
             faceId = upToFace.remove(0,4).toInt(&ok);
         }
         ui->lineFaceName->setText(ok ?
-                                  tr("Face") + QString::number(faceId) :
+                                  parts[0] + tr(":Face") + QString::number(faceId) :
                                   tr("No face selected"));
         ui->doubleSpinBox->blockSignals(false);
         ui->doubleSpinBox2->blockSignals(false);
@@ -391,15 +400,10 @@ bool TaskDlgPadParameters::accept()
         Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.Midplane = %i",name.c_str(),parameter->getMidplane()?1:0);
         Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.Length2 = %f",name.c_str(),parameter->getLength2());
         Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.Type = %u",name.c_str(),parameter->getMode());
-        std::string facename = parameter->getFaceName().data();
-        PartDesign::Pad* pcPad = static_cast<PartDesign::Pad*>(PadView->getObject());
-        Part::Feature* support = pcPad->getSupport();
+        std::string facename = parameter->getFaceName();
 
-        if (support != NULL && !facename.empty()) {
-            QString buf = QString::fromUtf8("(App.ActiveDocument.%1,[\"%2\"])");
-            buf = buf.arg(QString::fromUtf8(support->getNameInDocument()));
-            buf = buf.arg(QString::fromStdString(facename));
-            Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.UpToFace = %s", name.c_str(), buf.toStdString().c_str());
+        if (!facename.empty()) {
+            Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.UpToFace = %s", name.c_str(), facename.c_str());
         } else
             Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.UpToFace = None", name.c_str());
         Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.recompute()");

src/Mod/PartDesign/Gui/TaskPocketParameters.cpp
@@ -95,6 +95,7 @@ TaskPocketParameters::TaskPocketParameters(ViewProviderPocket *PocketView,QWidge
     bool midplane = pcPocket->Midplane.getValue();
     bool reversed = pcPocket->Reversed.getValue();
     int index = pcPocket->Type.getValue(); // must extract value here, clear() kills it!
+    App::DocumentObject* obj =  pcPocket->UpToFace.getValue();
     std::vector<std::string> subStrings = pcPocket->UpToFace.getSubValues();
     std::string upToFace;
     int faceId = -1;
@@ -110,9 +111,13 @@ TaskPocketParameters::TaskPocketParameters(ViewProviderPocket *PocketView,QWidge
     ui->doubleSpinBox->setValue(l);
     ui->checkBoxMidplane->setChecked(midplane);
     ui->checkBoxReversed->setChecked(reversed);
-    ui->lineFaceName->setText(faceId >= 0 ?
-                              tr("Face") + QString::number(faceId) :
-                              tr("No face selected"));
+    if (PartDesign::Feature::isDatum(obj))
+        ui->lineFaceName->setText(QString::fromAscii(obj->getNameInDocument()));
+    else if (faceId >= 0)
+        ui->lineFaceName->setText(QString::fromAscii(obj->getNameInDocument()) + tr("Face") +
+                                  QString::number(faceId));
+    else
+        ui->lineFaceName->setText(tr("No face selected"));
     ui->lineFaceName->setProperty("FaceName", QByteArray(upToFace.c_str()));
     ui->changeMode->clear();
     ui->changeMode->insertItem(0, tr("Dimension"));
@@ -179,7 +184,7 @@ void TaskPocketParameters::updateUI(int index)
         ui->lineFaceName->setEnabled(true);
         QMetaObject::invokeMethod(ui->lineFaceName, "setFocus", Qt::QueuedConnection);
         // Go into reference selection mode if no face has been selected yet
-        if (ui->lineFaceName->text().isEmpty())
+        if (ui->lineFaceName->text().isEmpty() || (ui->lineFaceName->text() == tr("No face selected")))
             onButtonFace(true);
     }
 }
@@ -301,9 +306,12 @@ int TaskPocketParameters::getMode(void) const
     return ui->changeMode->currentIndex();
 }
 
-QByteArray TaskPocketParameters::getFaceName(void) const
+const std::string TaskPocketParameters::getFaceName(void) const
 {
-    return ui->lineFaceName->property("FaceName").toByteArray();
+    if ((getMode() >= 1) || (getMode() <= 3))
+        return getFaceReference(ui->lineFaceName->text(), ui->lineFaceName->property("FaceName").toString()).toStdString();
+    else
+        return "";
 }
 
 const bool TaskPocketParameters::updateView() const
@@ -332,14 +340,15 @@ void TaskPocketParameters::changeEvent(QEvent *e)
         ui->changeMode->addItem(tr("Up to face"));
         ui->changeMode->setCurrentIndex(index);
 
-        QByteArray upToFace = this->getFaceName();
+        QStringList parts = ui->lineFaceName->text().split(QChar::fromAscii(':'));
+        QByteArray upToFace = ui->lineFaceName->property("FaceName").toByteArray();
         int faceId = -1;
         bool ok = false;
         if (upToFace.indexOf("Face") == 0) {
             faceId = upToFace.remove(0,4).toInt(&ok);
         }
         ui->lineFaceName->setText(ok ?
-                                  tr("Face") + QString::number(faceId) :
+                                  parts[0] + tr(":Face") + QString::number(faceId) :
                                   tr("No face selected"));
         ui->doubleSpinBox->blockSignals(false);
         ui->lineFaceName->blockSignals(false);
@@ -378,13 +387,9 @@ bool TaskDlgPocketParameters::accept()
         Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.Reversed = %i",name.c_str(),parameter->getReversed()?1:0);
         Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.Type = %u",name.c_str(),parameter->getMode());
         std::string facename = parameter->getFaceName().data();
-        PartDesign::Pocket* pcPocket = static_cast<PartDesign::Pocket*>(PocketView->getObject());
-        Part::Feature* support = pcPocket->getSupport();
-        if (support != NULL && !facename.empty()) {
-            QString buf = QString::fromUtf8("(App.ActiveDocument.%1,[\"%2\"])");
-            buf = buf.arg(QString::fromUtf8(support->getNameInDocument()));
-            buf = buf.arg(QString::fromStdString(facename));
-            Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.UpToFace = %s", name.c_str(), buf.toStdString().c_str());
+
+        if (!facename.empty()) {
+            Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.UpToFace = %s", name.c_str(), facename.c_str());
         } else
             Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.UpToFace = None", name.c_str());
         Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.recompute()");

southseact-3d/fcd

src/Mod/PartDesign/Gui/TaskPadParameters.cpp
@@ -103,6 +103,7 @@ TaskPadParameters::TaskPadParameters(ViewProviderPad *PadView,bool newObj, QWidg
     bool reversed = pcPad->Reversed.getValue();
     Base::Quantity l2 = pcPad->Length2.getQuantityValue();
     int index = pcPad->Type.getValue(); // must extract value here, clear() kills it!
+    App::DocumentObject* obj = pcPad->UpToFace.getValue();
     std::vector<std::string> subStrings = pcPad->UpToFace.getSubValues();
     std::string upToFace;
     int faceId = -1;
@@ -128,12 +129,13 @@ TaskPadParameters::TaskPadParameters(ViewProviderPad *PadView,bool newObj, QWidg
     // According to bug #0000521 the reversed option
     // shouldn't be de-activated if the pad has a support face
     ui->checkBoxReversed->setChecked(reversed);
-#if QT_VERSION >= 0x040700
-    ui->lineFaceName->setPlaceholderText(tr("No face selected"));
-#endif
-    ui->lineFaceName->setText(faceId >= 0 ?
-                              tr("Face") + QString::number(faceId) :
-                              QString());
+    if (PartDesign::Feature::isDatum(obj))
+        ui->lineFaceName->setText(QString::fromAscii(obj->getNameInDocument()));
+    else if (faceId >= 0)
+        ui->lineFaceName->setText(QString::fromAscii(obj->getNameInDocument()) + tr("Face") +
+                                  QString::number(faceId));
+    else
+        ui->lineFaceName->setText(tr("No face selected"));
     ui->lineFaceName->setProperty("FaceName", QByteArray(upToFace.c_str()));
     ui->changeMode->clear();
     ui->changeMode->insertItem(0, tr("Dimension"));
@@ -195,7 +197,7 @@ void TaskPadParameters::updateUI(int index)
         ui->lineFaceName->setEnabled(true);
         QMetaObject::invokeMethod(ui->lineFaceName, "setFocus", Qt::QueuedConnection);
         // Go into reference selection mode if no face has been selected yet
-        if (ui->lineFaceName->text().isEmpty())
+        if (ui->lineFaceName->text().isEmpty() || (ui->lineFaceName->text() == tr("No face selected")))
             onButtonFace(true);
     } else { // two dimensions
         ui->lengthEdit->setEnabled(true);
@@ -332,7 +334,10 @@ int TaskPadParameters::getMode(void) const
 
 QByteArray TaskPadParameters::getFaceName(void) const
 {
-    return ui->lineFaceName->property("FaceName").toByteArray();
+    if ((getMode() >= 1) || (getMode() <= 3))
+        return getFaceReference(ui->lineFaceName->text(), ui->lineFaceName->property("FaceName").toString()).toLatin1();
+    else
+        return "";
 }
 
 const bool TaskPadParameters::updateView() const
@@ -363,7 +368,8 @@ void TaskPadParameters::changeEvent(QEvent *e)
         ui->changeMode->addItem(tr("Two dimensions"));
         ui->changeMode->setCurrentIndex(index);
 
-        QByteArray upToFace = this->getFaceName();
+        QStringList parts = ui->lineFaceName->text().split(QChar::fromAscii(':'));
+        QByteArray upToFace = ui->lineFaceName->property("FaceName").toByteArray();
         int faceId = -1;
         bool ok = false;
         if (upToFace.indexOf("Face") == 0) {
@@ -373,7 +379,7 @@ void TaskPadParameters::changeEvent(QEvent *e)
         ui->lineFaceName->setPlaceholderText(tr("No face selected"));
 #endif
         ui->lineFaceName->setText(ok ?
-                                  tr("Face") + QString::number(faceId) :
+                                  parts[0] + tr(":Face") + QString::number(faceId) :
                                   QString());
         ui->lengthEdit->blockSignals(false);
         ui->lengthEdit2->blockSignals(false);
@@ -403,14 +409,9 @@ void TaskPadParameters::apply()
 
     Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.Type = %u",cname,getMode());
     std::string facename = getFaceName().data();
-    PartDesign::Pad* pcPad = static_cast<PartDesign::Pad*>(vp->getObject());
-    Part::Feature* support = pcPad->getSupport();
 
-    if (support != NULL && !facename.empty()) {
-        QString buf = QString::fromUtf8("(App.ActiveDocument.%1,[\"%2\"])");
-        buf = buf.arg(QString::fromUtf8(support->getNameInDocument()));
-        buf = buf.arg(QString::fromStdString(facename));
-        Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.UpToFace = %s", cname, buf.toStdString().c_str());
+    if (!facename.empty()) {
+        Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.UpToFace = %s", name.c_str(), facename.c_str());
     } else
         Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.UpToFace = None", cname);
     Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.recompute()");

src/Mod/PartDesign/Gui/TaskPocketParameters.cpp
@@ -96,6 +96,7 @@ TaskPocketParameters::TaskPocketParameters(ViewProviderPocket *PocketView,QWidge
     bool midplane = pcPocket->Midplane.getValue();
     bool reversed = pcPocket->Reversed.getValue();
     int index = pcPocket->Type.getValue(); // must extract value here, clear() kills it!
+    App::DocumentObject* obj =  pcPocket->UpToFace.getValue();
     std::vector<std::string> subStrings = pcPocket->UpToFace.getSubValues();
     std::string upToFace;
     int faceId = -1;
@@ -111,9 +112,13 @@ TaskPocketParameters::TaskPocketParameters(ViewProviderPocket *PocketView,QWidge
     ui->pocketLength->setValue(l);
     ui->checkBoxMidplane->setChecked(midplane);
     ui->checkBoxReversed->setChecked(reversed);
-    ui->lineFaceName->setText(faceId >= 0 ?
-                              tr("Face") + QString::number(faceId) :
-                              tr("No face selected"));
+    if (PartDesign::Feature::isDatum(obj))
+        ui->lineFaceName->setText(QString::fromAscii(obj->getNameInDocument()));
+    else if (faceId >= 0)
+        ui->lineFaceName->setText(QString::fromAscii(obj->getNameInDocument()) + tr("Face") +
+                                  QString::number(faceId));
+    else
+        ui->lineFaceName->setText(tr("No face selected"));
     ui->lineFaceName->setProperty("FaceName", QByteArray(upToFace.c_str()));
     ui->changeMode->clear();
     ui->changeMode->insertItem(0, tr("Dimension"));
@@ -185,7 +190,7 @@ void TaskPocketParameters::updateUI(int index)
         ui->lineFaceName->setEnabled(true);
         QMetaObject::invokeMethod(ui->lineFaceName, "setFocus", Qt::QueuedConnection);
         // Go into reference selection mode if no face has been selected yet
-        if (ui->lineFaceName->text().isEmpty())
+        if (ui->lineFaceName->text().isEmpty() || (ui->lineFaceName->text() == tr("No face selected")))
             onButtonFace(true);
     }
 }
@@ -309,7 +314,10 @@ int TaskPocketParameters::getMode(void) const
 
 QByteArray TaskPocketParameters::getFaceName(void) const
 {
-    return ui->lineFaceName->property("FaceName").toByteArray();
+    if ((getMode() >= 1) || (getMode() <= 3))
+        return getFaceReference(ui->lineFaceName->text(), ui->lineFaceName->property("FaceName").toString()).toLatin1();
+    else
+        return "";
 }
 
 const bool TaskPocketParameters::updateView() const
@@ -338,14 +346,15 @@ void TaskPocketParameters::changeEvent(QEvent *e)
         ui->changeMode->addItem(tr("Up to face"));
         ui->changeMode->setCurrentIndex(index);
 
-        QByteArray upToFace = this->getFaceName();
+        QStringList parts = ui->lineFaceName->text().split(QChar::fromAscii(':'));
+        QByteArray upToFace = ui->lineFaceName->property("FaceName").toByteArray();
         int faceId = -1;
         bool ok = false;
         if (upToFace.indexOf("Face") == 0) {
             faceId = upToFace.remove(0,4).toInt(&ok);
         }
         ui->lineFaceName->setText(ok ?
-                                  tr("Face") + QString::number(faceId) :
+                                  parts[0] + tr(":Face") + QString::number(faceId) :
                                   tr("No face selected"));
         ui->pocketLength->blockSignals(false);
         ui->lineFaceName->blockSignals(false);
@@ -361,13 +370,9 @@ void TaskPocketParameters::apply()
     ui->pocketLength->apply();
     Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.Type = %u",name.c_str(),getMode());
     std::string facename = getFaceName().data();
-    PartDesign::Pocket* pcPocket = static_cast<PartDesign::Pocket*>(vp->getObject());
-    Part::Feature* support = pcPocket->getSupport();
-    if (support != NULL && !facename.empty()) {
-        QString buf = QString::fromUtf8("(App.ActiveDocument.%1,[\"%2\"])");
-        buf = buf.arg(QString::fromUtf8(support->getNameInDocument()));
-        buf = buf.arg(QString::fromStdString(facename));
-        Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.UpToFace = %s", name.c_str(), buf.toStdString().c_str());
+
+    if (!facename.empty()) {
+        Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.UpToFace = %s", name.c_str(), facename.c_str());
     } else
         Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.UpToFace = None", name.c_str());
     Gui::Command::doCommand(Gui::Command::Doc,"App.ActiveDocument.%s.Reversed = %i", name.c_str(), getReversed()?1:0);