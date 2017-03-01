TinkerGnome/FreeCAD

py3: ported Part to python3

src/Mod/Part/Gui/AppPartGui.cpp
@@ -101,11 +101,11 @@ PyObject* initModule()
 
 } // namespace PartGui
 
-PyMODINIT_FUNC initPartGui()
+PyMOD_INIT_FUNC(PartGui)
 {
     if (!Gui::Application::Instance) {
         PyErr_SetString(PyExc_ImportError, "Cannot load Gui module in console application.");
-        return;
+        PyMOD_Return(0);
     }
 
     // load needed modules
@@ -114,10 +114,10 @@ PyMODINIT_FUNC initPartGui()
     }
     catch(const Base::Exception& e) {
         PyErr_SetString(PyExc_ImportError, e.what());
-        return;
+        PyMOD_Return(0);
     }
 
-    (void)PartGui::initModule();
+    PyObject* mod = PartGui::initModule();
     Base::Console().Log("Loading GUI of Part module... done\n");
 
     PartGui::SoBrepFaceSet                  ::initClass();
@@ -193,4 +193,6 @@ PyMODINIT_FUNC initPartGui()
     Gui::BitmapFactoryInst& rclBmpFactory = Gui::BitmapFactory();
     rclBmpFactory.addXPM("PartFeature",(const char**) PartFeature_xpm);
     rclBmpFactory.addXPM("PartFeatureImport",(const char**) PartFeatureImport_xpm);
+
+    PyMOD_Return(mod);
 }

southseact-3d/fcd

src/Mod/Part/Gui/AppPartGui.cpp
@@ -102,11 +102,11 @@ PyObject* initModule()
 
 } // namespace PartGui
 
-PyMODINIT_FUNC initPartGui()
+PyMOD_INIT_FUNC(PartGui)
 {
     if (!Gui::Application::Instance) {
         PyErr_SetString(PyExc_ImportError, "Cannot load Gui module in console application.");
-        return;
+        PyMOD_Return(0);
     }
 
     // load needed modules
@@ -115,10 +115,11 @@ PyMODINIT_FUNC initPartGui()
     }
     catch(const Base::Exception& e) {
         PyErr_SetString(PyExc_ImportError, e.what());
-        return;
+        PyMOD_Return(0);
     }
 
     PyObject* partGuiModule = PartGui::initModule();
+
     Base::Console().Log("Loading GUI of Part module... done\n");
 
     PyObject* pAttachEngineTextsModule = Py_InitModule3("AttachEngineResources", AttacherGui::AttacherGuiPy::Methods,
@@ -207,4 +208,6 @@ PyMODINIT_FUNC initPartGui()
     Gui::BitmapFactoryInst& rclBmpFactory = Gui::BitmapFactory();
     rclBmpFactory.addXPM("PartFeature",(const char**) PartFeature_xpm);
     rclBmpFactory.addXPM("PartFeatureImport",(const char**) PartFeatureImport_xpm);
+
+    PyMOD_Return(partGuiModule);
 }