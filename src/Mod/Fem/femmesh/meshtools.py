southseact-3d/fcd

commit faa1873cabe22d991d65a31a8866b6c41aa2755d

FEM: mesh tools, typo

src/Mod/Fem/femmesh/meshtools.py
@@ -671,7 +671,7 @@ def get_femmesh_groupdata_sets_by_name(
     # no check is done in this regard !!!
     # we just check for the group name and the group data type
     # what happens if a reference shape was changed
-    # but the mesh and the mesh groups were not created new !?!
+    # but the mesh and the mesh groups where not created new !?!
     obj = fem_object["Object"]
     if femmesh.GroupCount:
         for g in femmesh.Groups:

commit c6a1cb8abafab0f69d9a79debb464388ae3b90d2

FEM: fixed recent typo introduced in faa1873cabe22d

src/Mod/Fem/femmesh/meshtools.py
@@ -671,7 +671,7 @@ def get_femmesh_groupdata_sets_by_name(
     # no check is done in this regard !!!
     # we just check for the group name and the group data type
     # what happens if a reference shape was changed
-    # but the mesh and the mesh groups where not created new !?!
+    # but the mesh and the mesh groups were not created new !?!
     obj = fem_object["Object"]
     if femmesh.GroupCount:
         for g in femmesh.Groups: