southseact-3d/fcd

commit 11e29ff365b6213f5524c6e6d0a82c33808263c4

App: improve document::readObjects()

src/App/Document.cpp
@@ -1982,7 +1982,7 @@ Document::readObjects(Base::XMLReader& reader)
         reader.readElement("Object");
         std::string type = reader.getAttribute("type");
         std::string name = reader.getAttribute("name");
-        const char *viewType = reader.hasAttribute("ViewType")?reader.getAttribute("ViewType"):0;
+        std::string viewType = reader.hasAttribute("ViewType")?reader.getAttribute("ViewType"):"";
 
         bool partial = false;
         if(d->partialLoadObjects.size()) {
@@ -2019,7 +2019,7 @@ Document::readObjects(Base::XMLReader& reader)
             // otherwise we may cause a dependency to itself
             // Example: Object 'Cut001' references object 'Cut' and removing the
             // digits we make an object 'Cut' referencing itself.
-            App::DocumentObject* obj = addObject(type.c_str(), obj_name, /*isNew=*/ false, viewType, partial);
+            App::DocumentObject* obj = addObject(type.c_str(), obj_name, /*isNew=*/ false, viewType.c_str(), partial);
             if (obj) {
                 if(lastId < obj->_Id)
                     lastId = obj->_Id;

commit cf347f93724aedd6674584ace1007488eeae7275

fix regression caused by commit 11e29ff36

src/App/Document.cpp
@@ -3537,9 +3537,10 @@ DocumentObject * Document::addObject(const char* sType, const char* pObjectName,
 
     pcObject->setStatus(ObjectStatus::PartialObject, isPartial);
 
-    if(!viewType)
+    if (!viewType || viewType[0] == '\0')
         viewType = pcObject->getViewProviderNameOverride();
-    if(viewType) 
+
+    if (viewType && viewType[0] != '\0')
         pcObject->_pcViewProviderName = viewType;
 
     signalNewObject(*pcObject);