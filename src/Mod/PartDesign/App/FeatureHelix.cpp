southseact-3d/fcd

commit a79239e55b6cacf7df630985b959ca3225938ce4

[PD] small code improvements for Helix

src/Mod/PartDesign/App/FeatureHelix.cpp
@@ -85,11 +85,11 @@ Helix::Helix()
         "The helix' direction; derived from the reference axis.");
     ADD_PROPERTY_TYPE(ReferenceAxis, (0), group, App::Prop_None,
         "The reference axis of the helix.");
-    ADD_PROPERTY_TYPE(Mode, (long(initialMode)), group, App::Prop_None,
+    ADD_PROPERTY_TYPE(Mode, (unsigned int(initialMode)), group, App::Prop_None,
         "The helix input mode specifies which properties are set by the user.\n"
         "Dependent properties are then calculated.");
     Mode.setEnums(ModeEnums);
-    ADD_PROPERTY_TYPE(Pitch, (10.), group, App::Prop_None,
+    ADD_PROPERTY_TYPE(Pitch, (10.0), group, App::Prop_None,
         "The axial distance between two turns.");
     ADD_PROPERTY_TYPE(Height, (30.0), group, App::Prop_None,
         "The height of the helix' path, not accounting for the extent of the profile.");
@@ -104,14 +104,14 @@ Helix::Helix()
     ADD_PROPERTY_TYPE(Growth, (0.0), group, App::Prop_None,
         "The growth of the helix' radius per turn.\n"
         "Non-zero values turn the helix into a conical spiral.");
-    ADD_PROPERTY_TYPE(LeftHanded, (long(0)), group, App::Prop_None,
+    ADD_PROPERTY_TYPE(LeftHanded, (false), group, App::Prop_None,
         "Sets the turning direction to left handed,\n"
         "i.e. counter-clockwise when moving along its axis.");
-    ADD_PROPERTY_TYPE(Reversed, (long(0)), group, App::Prop_None,
+    ADD_PROPERTY_TYPE(Reversed, (false), group, App::Prop_None,
         "Determines whether the helix points in the opposite direction of the axis.");
-    ADD_PROPERTY_TYPE(Outside, (long(0)), group, App::Prop_None,
+    ADD_PROPERTY_TYPE(Outside, (false), group, App::Prop_None,
         "If set, the result will be the intersection of the profile and the preexisting body.");
-    ADD_PROPERTY_TYPE(HasBeenEdited, (long(0)), group, App::Prop_Hidden,
+    ADD_PROPERTY_TYPE(HasBeenEdited, (false), group, App::Prop_Hidden,
         "If false, the tool will propose an initial value for the pitch based on the profile bounding box,\n"
         "so that self intersection is avoided.");
 

commit e605e0191664419dc98dd4b7ea484838633d1d8e

[PD] fix compilation issue with Helix
I introduced it with commit a79239e5

src/Mod/PartDesign/App/FeatureHelix.cpp
@@ -85,7 +85,7 @@ Helix::Helix()
         "The helix' direction; derived from the reference axis.");
     ADD_PROPERTY_TYPE(ReferenceAxis, (0), group, App::Prop_None,
         "The reference axis of the helix.");
-    ADD_PROPERTY_TYPE(Mode, (unsigned int(initialMode)), group, App::Prop_None,
+    ADD_PROPERTY_TYPE(Mode, (long(initialMode)), group, App::Prop_None,
         "The helix input mode specifies which properties are set by the user.\n"
         "Dependent properties are then calculated.");
     Mode.setEnums(ModeEnums);