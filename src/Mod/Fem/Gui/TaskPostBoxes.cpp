southseact-3d/fcd

commit 78164d158d9515d4ebe0a033e7685360f11fb07c

[FEM] fix bug 4 of the cut filter bugs
as reported here: https://forum.freecadweb.org/viewtopic.php?p=584217#p584217

src/Mod/Fem/Gui/TaskPostBoxes.cpp
@@ -463,12 +463,17 @@ void TaskPostClip::collectImplicitFunctions() {
 
             ui->FunctionBox->clear();
             QStringList items;
+            std::size_t currentItem = 0;
+            App::DocumentObject* currentFunction = static_cast<Fem::FemPostClipFilter*>(getObject())->Function.getValue();
             const std::vector<App::DocumentObject*>& funcs = static_cast<Fem::FemPostFunctionProvider*>(
                 pipeline->Functions.getValue())->Functions.getValues();
-            for (std::size_t i = 0; i < funcs.size(); ++i)
+            for (std::size_t i = 0; i < funcs.size(); ++i) {
                 items.push_back(QString::fromLatin1(funcs[i]->getNameInDocument()));
-
+                if (currentFunction = funcs[i])
+                    currentItem = i;
+            }
             ui->FunctionBox->addItems(items);
+            ui->FunctionBox->setCurrentIndex(currentItem);
         }
     }
 }

commit f2e8791f5b059c92ceed9574401130d740a8fbad

[FEM] fix a comparison operator
Fixes a mistake I introduced with https://github.com/FreeCAD/FreeCAD/commit/78164d158d95

src/Mod/Fem/Gui/TaskPostBoxes.cpp
@@ -469,7 +469,7 @@ void TaskPostClip::collectImplicitFunctions() {
                 pipeline->Functions.getValue())->Functions.getValues();
             for (std::size_t i = 0; i < funcs.size(); ++i) {
                 items.push_back(QString::fromLatin1(funcs[i]->getNameInDocument()));
-                if (currentFunction = funcs[i])
+                if (currentFunction == funcs[i])
                     currentItem = i;
             }
             ui->FunctionBox->addItems(items);