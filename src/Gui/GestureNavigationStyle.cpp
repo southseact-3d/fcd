southseact-3d/fcd

commit 63a6b2e11f41ba39bb26a1442af752191556d39c

[Gui] remove unnecessary Boolean comparisons

src/Gui/GestureNavigationStyle.cpp
@@ -893,9 +893,9 @@ SbBool GestureNavigationStyle::processSoEvent(const SoEvent* const ev)
             return true;
     }
 
-    if (   (smev.isRelease(1) && this->button1down == false)
-        || (smev.isRelease(2) && this->button2down == false)
-        || (smev.isRelease(3) && this->button3down == false)) {
+    if (   (!smev.isRelease(1) && this->button1down)
+        || (!smev.isRelease(2) && this->button2down)
+        || (!smev.isRelease(3) && this->button3down)) {
         //a button release event cane, but we didn't see the corresponding down
         //event. Discard it. This discarding is relied upon in some hacks to
         //overcome buggy synthetic mouse input coming from Qt when doing

src/Gui/Application.cpp
@@ -546,7 +546,7 @@ void Application::open(const char* FileName, const char* Module)
     // in case of an automatically created empty document at startup
     App::Document* act = App::GetApplication().getActiveDocument();
     Gui::Document* gui = this->getDocument(act);
-    if (act && act->countObjects() == 0 && gui && gui->isModified() == false){
+    if (act && act->countObjects() == 0 && gui && !gui->isModified()){
         Command::doCommand(Command::App, "App.closeDocument('%s')", act->getName());
         qApp->processEvents(); // an update is needed otherwise the new view isn't shown
     }
@@ -2410,7 +2410,7 @@ void Application::setStyleSheet(const QString& qssFile, bool tiledBackground)
     // appear incorrect due to an outdated cache.
     // See https://doc.qt.io/qt-5/qstyle.html#unpolish-1
     // See https://forum.freecadweb.org/viewtopic.php?f=17&t=50783
-    if (d->startingUp == false) {
+    if (!d->startingUp) {
         if (mdi->style())
             mdi->style()->unpolish(qApp);
     }

src/Gui/CommandView.cpp
@@ -2152,7 +2152,7 @@ void StdCmdAxisCross::activated(int iMsg)
     Q_UNUSED(iMsg);
     Gui::View3DInventor* view = qobject_cast<View3DInventor*>(Gui::getMainWindow()->activeWindow());
     if (view) {
-        if (view->getViewer()->hasAxisCross() == false)
+        if (!view->getViewer()->hasAxisCross())
             doCommand(Command::Gui,"Gui.ActiveDocument.ActiveView.setAxisCross(True)");
         else
             doCommand(Command::Gui,"Gui.ActiveDocument.ActiveView.setAxisCross(False)");

src/Gui/Document.cpp
@@ -1356,7 +1356,7 @@ unsigned int Document::getMemSize (void) const
 void Document::Save (Base::Writer &writer) const
 {
     // It's only possible to add extra information if force of XML is disabled
-    if (writer.isForceXML() == false) {
+    if (!writer.isForceXML()) {
         writer.addFile("GuiDocument.xml", this);
 
         ParameterGrp::handle hGrp = App::GetApplication().GetParameterGroupByPath("User parameter:BaseApp/Preferences/Document");
@@ -1909,7 +1909,7 @@ void Document::detachView(Gui::BaseView* pcView, bool bPassiv)
             }
 
             // is already closing the document, and is not linked by other documents
-            if (d->_isClosing == false &&
+            if (!d->_isClosing &&
                 App::PropertyXLink::getDocumentInList(getDocument()).empty())
             {
                 d->_pcAppWnd->onLastWindowClosed(this);

src/Gui/GuiApplicationNativeEventAware.cpp
@@ -293,10 +293,10 @@ void Gui::GUIApplicationNativeEventAware::importSettings(std::vector<int>& motio
 
     for (i = 0; i < 6; ++i) {
         if (motionDataArray[i] != 0) {
-            if (enabled[i] == false)
+            if (!enabled[i])
                 motionDataArray[i] = 0;
             else {
-                if (reversed[i] == true)
+                if (reversed[i])
                     motionDataArray[i] = - motionDataArray[i];
                 motionDataArray[i] = (int)((float)(motionDataArray[i]) * sensitivity[i] * generalSensitivity);
             }

src/Gui/NetworkRetriever.cpp
@@ -261,9 +261,9 @@ bool NetworkRetriever::startDownload( const QString& startUrl )
     if ( !d->dir.isEmpty() )
     {
         QDir dir(d->dir);
-        if ( dir.exists( d->dir ) == false )
+        if (!dir.exists(d->dir))
         {
-            if ( dir.mkdir( d->dir ) == false)
+            if (!dir.mkdir(d->dir))
             {
                 Base::Console().Error("Directory '%s' could not be created.", (const char*)d->dir.toLatin1());
                 return true; // please, no error message
@@ -534,7 +534,7 @@ void StdCmdDownloadOnlineHelp::activated(int iMsg)
 
         if (canStart) {
             bool ok = wget->startDownload(QString::fromLatin1(url.c_str()));
-            if ( ok == false )
+            if (!ok)
                 Base::Console().Error("The tool 'wget' couldn't be found. Please check your installation.");
             else if ( wget->isDownloading() && _pcAction )
                 _pcAction->setText(tr("Stop downloading"));

src/Gui/PythonConsole.cpp
@@ -971,7 +971,7 @@ void PythonConsole::mouseReleaseEvent( QMouseEvent *e )
   if (e->button() == Qt::LeftButton)
   {
     QTextCursor cursor   = this->textCursor();
-    if (cursor.hasSelection() == false
+    if (!cursor.hasSelection()
      && cursor < this->inputBegin())
     {
       cursor.movePosition( QTextCursor::End );

src/Gui/SplitView3DInventor.cpp
@@ -297,7 +297,7 @@ void AbstractSplitView::OnChange(ParameterGrp::SubjectType &rCaller,ParameterGrp
         r4 = ((col4 >> 24) & 0xff) / 255.0; g4 = ((col4 >> 16) & 0xff) / 255.0; b4 = ((col4 >> 8) & 0xff) / 255.0;
         for (std::vector<View3DInventorViewer*>::iterator it = _viewer.begin(); it != _viewer.end(); ++it) {
             (*it)->setBackgroundColor(QColor::fromRgbF(r1, g1, b1));
-            if (rGrp.GetBool("UseBackgroundColorMid",false) == false)
+            if (!rGrp.GetBool("UseBackgroundColorMid",false))
                 (*it)->setGradientBackgroundColor(SbColor(r2, g2, b2), SbColor(r3, g3, b3));
             else
                 (*it)->setGradientBackgroundColor(SbColor(r2, g2, b2), SbColor(r3, g3, b3), SbColor(r4, g4, b4));

commit a13c8092d9872ab4fe58d7ed1d84c4a95e4c367f

[BUGFIX] Gui: fix mouse Gesture navigation mode
 Regression introduced by commit 63a6b2e

src/Gui/GestureNavigationStyle.cpp
@@ -893,9 +893,9 @@ SbBool GestureNavigationStyle::processSoEvent(const SoEvent* const ev)
             return true;
     }
 
-    if (   (!smev.isRelease(1) && this->button1down)
-        || (!smev.isRelease(2) && this->button2down)
-        || (!smev.isRelease(3) && this->button3down)) {
+    if (   (smev.isRelease(1) && !this->button1down)
+        || (smev.isRelease(2) && !this->button2down)
+        || (smev.isRelease(3) && !this->button3down)) {
         //a button release event cane, but we didn't see the corresponding down
         //event. Discard it. This discarding is relied upon in some hacks to
         //overcome buggy synthetic mouse input coming from Qt when doing