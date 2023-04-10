southseact-3d/fcd

commit c72ecb180542f99a3dfba4f234deb474a8eedc00

Sketcher: do not apply line/edge color preference to existing sketches, fixes #8188

src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
@@ -221,10 +221,6 @@ void ViewProviderSketch::ParameterObserver::initParameters()
             {[this](const std::string & string, App::Property * property){ updateBoolProperty(string, property, true);}, &Client.Autoconstraints }},
         {"AvoidRedundantAutoconstraints",
             {[this](const std::string & string, App::Property * property){ updateBoolProperty(string, property, true);}, &Client.AvoidRedundant }},
-        {"SketchEdgeColor",
-            {[this](const std::string & string, App::Property * property){ updateColorProperty(string, property, 1.f, 1.f, 1.f);}, &Client.LineColor }},
-        {"SketchVertexColor",
-            {[this](const std::string & string, App::Property * property){ updateColorProperty(string, property, 1.f, 1.f, 1.f);}, &Client.PointColor }},
         {"updateEscapeKeyBehaviour",
             {[this](const std::string & string, App::Property * property){ updateEscapeKeyBehaviour(string, property);}, nullptr }},
         {"AutoRecompute",

commit ae2e84a05a10a174bed505c411a43516c3aeeaef

[Bugfix]Sketcher: edge/point colors in preferences aren't took into account
 Regression introduced by c72ecb1 (PR #9151)

src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
@@ -260,6 +260,8 @@ void ViewProviderSketch::ParameterObserver::initParameters()
     }
 
     // unsubscribed parameters which update a property on just once upon construction (and before restore if properties are being restored from a file)
+    updateColorProperty("SketchEdgeColor", &Client.LineColor, 1.0f, 1.0f, 1.0f);
+    updateColorProperty("SketchVertexColor", &Client.PointColor, 1.0f, 1.0f, 1.0f);
     updateBoolProperty("ShowGrid", &Client.ShowGrid, false);
     updateBoolProperty("GridAuto", &Client.GridAuto, true);
     updateGridSize("GridSize", &Client.GridSize);