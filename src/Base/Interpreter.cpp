southseact-3d/fcd

commit b0c4c10eb23e1294e3b9ee6df4c2a531414a4abd

Base: Do not use else before return

src/Base/Interpreter.cpp
@@ -475,14 +475,11 @@ void InterpreterSingleton::runFile(const char* pxFileName, bool local)
             if (PyErr_ExceptionMatches(PyExc_SystemExit)) {
                 throw SystemExitException();
             }
-
             throw PyException();
         }
         Py_DECREF(result);
     }
-    else {
-        throw FileException("Unknown file", pxFileName);
-    }
+    throw FileException("Unknown file", pxFileName);
 }
 
 bool InterpreterSingleton::loadModule(const char* psModName)

src/Base/MatrixPyImp.cpp
@@ -256,11 +256,9 @@ PyObject* MatrixPy::richCompare(PyObject* v, PyObject* w, int op)
         Py_INCREF(res);
         return res;
     }
-    else {
-        // This always returns False
-        Py_INCREF(Py_NotImplemented);
-        return Py_NotImplemented;
-    }
+    // This always returns False
+    Py_INCREF(Py_NotImplemented);
+    return Py_NotImplemented;
 }
 
 PyObject* MatrixPy::move(PyObject* args)

src/Base/Parameter.cpp
@@ -501,11 +501,7 @@ std::vector<Base::Reference<ParameterGrp>> ParameterGrp::GetGroups()
 /// test if this group is empty
 bool ParameterGrp::IsEmpty() const
 {
-    if (_pGroupNode && _pGroupNode->getFirstChild()) {
-        return false;
-    }
-
-    return true;
+    return !(_pGroupNode && _pGroupNode->getFirstChild());
 }
 
 /// test if a special sub group is in this group

src/Base/PlacementPyImp.cpp
@@ -148,22 +148,18 @@ PyObject* PlacementPy::richCompare(PyObject* v, PyObject* w, int op)
             PyErr_SetString(PyExc_TypeError, "no ordering relation is defined for Placement");
             return nullptr;
         }
-        else if (op == Py_EQ) {
+        if (op == Py_EQ) {
             res = (p1 == p2) ? Py_True : Py_False;
             Py_INCREF(res);
             return res;
         }
-        else {
-            res = (p1 != p2) ? Py_True : Py_False;
-            Py_INCREF(res);
-            return res;
-        }
+        res = (p1 != p2) ? Py_True : Py_False;
+        Py_INCREF(res);
+        return res;
     }
-    else {
-        // This always returns False
-        Py_INCREF(Py_NotImplemented);
-        return Py_NotImplemented;
-    }
+    // This always returns False
+    Py_INCREF(Py_NotImplemented);
+    return Py_NotImplemented;
 }
 
 PyObject* PlacementPy::move(PyObject* args)

src/Base/PyObjectBase.cpp
@@ -365,7 +365,7 @@ int PyObjectBase::__setattro(PyObject *obj, PyObject *attro, PyObject *value)
         PyErr_Format(PyExc_AttributeError, "Cannot delete attribute: '%s'", attr);
         return -1;
     }
-    else if (!static_cast<PyObjectBase*>(obj)->isValid()){
+    if (!static_cast<PyObjectBase*>(obj)->isValid()){
         PyErr_Format(PyExc_ReferenceError, "Cannot access attribute '%s' of deleted object", attr);
         return -1;
     }
@@ -404,36 +404,32 @@ PyObject *PyObjectBase::_getattr(const char *attr)
         // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
         return reinterpret_cast<PyObject *>(Py_TYPE(this));
     }
-    else if (streq(attr, "__members__")) {
+    if (streq(attr, "__members__")) {
         // Use __dict__ instead as __members__ is deprecated
         return nullptr;
     }
-    else if (streq(attr,"__dict__")) {
+    if (streq(attr,"__dict__")) {
         // Return the default dict
         PyTypeObject *tp = Py_TYPE(this);
         Py_XINCREF(tp->tp_dict);
         return tp->tp_dict;
     }
-    else if (streq(attr,"softspace")) {
+    if (streq(attr,"softspace")) {
         // Internal Python stuff
         return nullptr;
     }
-    else {
-        // As fallback solution use Python's default method to get generic attributes
-        PyObject *w{};
-        PyObject *res{};
-        w = PyUnicode_InternFromString(attr);
-        if (w) {
-            res = PyObject_GenericGetAttr(this, w);
-            Py_XDECREF(w);
-            return res;
-        } else {
-            // Throw an exception for unknown attributes
-            PyTypeObject *tp = Py_TYPE(this);
-            PyErr_Format(PyExc_AttributeError, "%.50s instance has no attribute '%.400s'", tp->tp_name, attr);
-            return nullptr;
-        }
+    // As fallback solution use Python's default method to get generic attributes
+    PyObject *w{}, *res{};
+    w = PyUnicode_InternFromString(attr);
+    if (w) {
+        res = PyObject_GenericGetAttr(this, w);
+        Py_XDECREF(w);
+        return res;
     }
+    // Throw an exception for unknown attributes
+    PyTypeObject *tp = Py_TYPE(this);
+    PyErr_Format(PyExc_AttributeError, "%.50s instance has no attribute '%.400s'", tp->tp_name, attr);
+    return nullptr;
 }
 
 int PyObjectBase::_setattr(const char *attr, PyObject *value)
@@ -449,12 +445,11 @@ int PyObjectBase::_setattr(const char *attr, PyObject *value)
         int res = PyObject_GenericSetAttr(this, w, value);
         Py_DECREF(w);
         return res;
-    } else {
-        // Throw an exception for unknown attributes
-        PyTypeObject *tp = Py_TYPE(this);
-        PyErr_Format(PyExc_AttributeError, "%.50s instance has no attribute '%.400s'", tp->tp_name, attr);
-        return -1;
     }
+    // Throw an exception for unknown attributes
+    PyTypeObject *tp = Py_TYPE(this);
+    PyErr_Format(PyExc_AttributeError, "%.50s instance has no attribute '%.400s'", tp->tp_name, attr);
+    return -1;
 }
 
 /*------------------------------

src/Base/PyTools.c
@@ -359,27 +359,25 @@ PP_Convert_Result(PyObject *presult, const char *resFormat, void *resTarget)
         Py_DECREF(presult);             /* procedures and stmts return None  */
         return 0;
     }
-    else
     if (! PyArg_Parse(presult, resFormat, resTarget)) {  /* convert Python->C */
         Py_DECREF(presult);                              /* need not be tuple */
         return -1;                                       /* error in convert  */
     }
-    else {
-        if (strcmp(resFormat, "O") != 0) {     /* free object unless exported */
-            if (strcmp(resFormat, "s") == 0) { /* copy string: caller owns it */
-                char **target = (char**) resTarget;
+    if (strcmp(resFormat, "O") != 0) {     /* free object unless exported */
+        if (strcmp(resFormat, "s") == 0) { /* copy string: caller owns it */
+            char **target = (char**) resTarget;
 #if defined (__GNUC__)
-                *target = strdup(*target);
+            *target = strdup(*target);
 #else
-                *target = _strdup(*target);
+            *target = _strdup(*target);
 #endif
-            }
-            Py_DECREF(presult);
         }
-        return 0;                     /* returns 0=success, -1=failure */
-    }                                 /* if 0: C result in *resTarget  */
-}                                     /* caller must decref if fmt="O" */
-                                      /* caller must free() if fmt="s" */
+        Py_DECREF(presult);
+    }
+    return 0;                     /* returns 0=success, -1=failure */
+                                  /* if 0: C result in *resTarget  */
+}                                 /* caller must decref if fmt="O" */
+                                  /* caller must free() if fmt="s" */
 
 int
 PP_Get_Global(const char *modname, const char *varname, const char *resfmt, void *cresult)
@@ -430,27 +428,26 @@ int PP_DEBUG  = 0;    /* debug embedded code with pdb? */
 
 const char *PP_Init(const char *modname) {
     Py_Initialize();                               /* init python if needed */
-    if (modname!=NULL) return modname;
-    { /* we assume here that the caller frees allocated memory */
-    	return "__main__";
-    }
+    if (modname)
+        return modname;
+    /* we assume here that the caller frees allocated memory */
+    return "__main__";
 }
 
 
 int
-PP_Make_Dummy_Module(const char *modname)   /* namespace for strings, if no file */
-{                                     /* instead of sharing __main__ for all */
-    PyObject *module = NULL, *dict = NULL;          /* note: __main__ is created in py_init */
+PP_Make_Dummy_Module(const char *modname)    /* namespace for strings, if no file */
+{                                            /* instead of sharing __main__ for all */
+    PyObject *module = NULL, *dict = NULL;   /* note: __main__ is created in py_init */
     Py_Initialize();
     module = PyImport_AddModule(modname);    /* fetch or make, no load */
     if (module == NULL)                      /* module not incref'd */
         return -1;
-    else {                                            /* module.__dict__ */
-        dict = PyModule_GetDict(module);              /* ['__dummy__'] = None */
-        PyDict_SetItemString(dict, "__dummy__", Py_None);
-        PyDict_SetItemString(dict, "__builtins__", PyEval_GetBuiltins());
-        return 0;
-    }
+    /* module.__dict__ */
+    dict = PyModule_GetDict(module);         /* ['__dummy__'] = None */
+    PyDict_SetItemString(dict, "__dummy__", Py_None);
+    PyDict_SetItemString(dict, "__builtins__", PyEval_GetBuiltins());
+    return 0;
 }
 
 
@@ -479,17 +476,14 @@ PP_Load_Module(const char *modname)       /* modname can be "package.module" for
         PyDict_GetItemString(PyModule_GetDict(module), "__dummy__")) {
         return module;                                /* not increfd */
     }
-    else
     if (PP_RELOAD && module != NULL && PyModule_Check(module)) {
         module = PyImport_ReloadModule(module);       /* reload file,run code */
         Py_XDECREF(module);                           /* still on sys.modules */
         return module;                                /* not increfd */
     }
-    else {
-        module = PyImport_ImportModule(modname);      /* fetch or load module */
-        Py_XDECREF(module);                           /* still on sys.modules */
-        return module;                                /* not increfd */
-    }
+    module = PyImport_ImportModule(modname);          /* fetch or load module */
+    Py_XDECREF(module);                               /* still on sys.modules */
+    return module;                                    /* not increfd */
 }
 
 

src/Base/QuantityPyImp.cpp
@@ -508,20 +508,18 @@ PyObject* QuantityPy::number_power_handler(PyObject* self, PyObject* other, PyOb
 
             return new QuantityPy(new Quantity(q));
         }
-        else if (PyFloat_Check(other)) {
+        if (PyFloat_Check(other)) {
             Base::Quantity* a = static_cast<QuantityPy*>(self)->getQuantityPtr();
             double b = PyFloat_AsDouble(other);
             return new QuantityPy(new Quantity(a->pow(b)));
         }
-        else if (PyLong_Check(other)) {
+        if (PyLong_Check(other)) {
             Base::Quantity* a = static_cast<QuantityPy*>(self)->getQuantityPtr();
             double b = (double)PyLong_AsLong(other);
             return new QuantityPy(new Quantity(a->pow(b)));
         }
-        else {
-            PyErr_SetString(PyExc_TypeError, "Expected quantity or number");
-            return nullptr;
-        }
+        PyErr_SetString(PyExc_TypeError, "Expected quantity or number");
+        return nullptr;
     }
     PY_CATCH
 }

src/Base/Reader.cpp
@@ -116,12 +116,10 @@ long Base::XMLReader::getAttributeAsInteger(const char* AttrName) const
     if (pos != AttrMap.end()) {
         return atol(pos->second.c_str());
     }
-    else {
-        // wrong name, use hasAttribute if not sure!
-        std::ostringstream msg;
-        msg << "XML Attribute: \"" << AttrName << "\" not found";
-        throw Base::XMLAttributeError(msg.str());
-    }
+    // wrong name, use hasAttribute if not sure!
+    std::ostringstream msg;
+    msg << "XML Attribute: \"" << AttrName << "\" not found";
+    throw Base::XMLAttributeError(msg.str());
 }
 
 unsigned long Base::XMLReader::getAttributeAsUnsigned(const char* AttrName) const
@@ -131,12 +129,10 @@ unsigned long Base::XMLReader::getAttributeAsUnsigned(const char* AttrName) cons
     if (pos != AttrMap.end()) {
         return strtoul(pos->second.c_str(), nullptr, 10);
     }
-    else {
-        // wrong name, use hasAttribute if not sure!
-        std::ostringstream msg;
-        msg << "XML Attribute: \"" << AttrName << "\" not found";
-        throw Base::XMLAttributeError(msg.str());
-    }
+    // wrong name, use hasAttribute if not sure!
+    std::ostringstream msg;
+    msg << "XML Attribute: \"" << AttrName << "\" not found";
+    throw Base::XMLAttributeError(msg.str());
 }
 
 double Base::XMLReader::getAttributeAsFloat(const char* AttrName) const
@@ -146,12 +142,10 @@ double Base::XMLReader::getAttributeAsFloat(const char* AttrName) const
     if (pos != AttrMap.end()) {
         return atof(pos->second.c_str());
     }
-    else {
-        // wrong name, use hasAttribute if not sure!
-        std::ostringstream msg;
-        msg << "XML Attribute: \"" << AttrName << "\" not found";
-        throw Base::XMLAttributeError(msg.str());
-    }
+    // wrong name, use hasAttribute if not sure!
+    std::ostringstream msg;
+    msg << "XML Attribute: \"" << AttrName << "\" not found";
+    throw Base::XMLAttributeError(msg.str());
 }
 
 const char* Base::XMLReader::getAttribute(const char* AttrName) const
@@ -161,12 +155,10 @@ const char* Base::XMLReader::getAttribute(const char* AttrName) const
     if (pos != AttrMap.end()) {
         return pos->second.c_str();
     }
-    else {
-        // wrong name, use hasAttribute if not sure!
-        std::ostringstream msg;
-        msg << "XML Attribute: \"" << AttrName << "\" not found";
-        throw Base::XMLAttributeError(msg.str());
-    }
+    // wrong name, use hasAttribute if not sure!
+    std::ostringstream msg;
+    msg << "XML Attribute: \"" << AttrName << "\" not found";
+    throw Base::XMLAttributeError(msg.str());
 }
 
 bool Base::XMLReader::hasAttribute(const char* AttrName) const
@@ -216,7 +208,7 @@ void Base::XMLReader::readElement(const char* ElementName)
             // thus we must stop reading on.
             break;
         }
-        else if (ReadType == EndDocument) {
+        if (ReadType == EndDocument) {
             // the end of the document has been reached but we still try to continue on reading
             throw Base::XMLParseException("End of document reached");
         }
@@ -276,7 +268,7 @@ void Base::XMLReader::readEndElement(const char* ElementName, int level)
         && (level < 0 || level == Level)) {
         return;
     }
-    else if (ReadType == EndDocument) {
+    if (ReadType == EndDocument) {
         // the end of the document has been reached but we still try to continue on reading
         throw Base::XMLParseException("End of document reached");
     }

commit c9c52a4f27e6e716a75699943d946639f34e0119

Base: Fix #12061
While rebasing after 2ab988fad4c0 ("fix readability-*:") a mistake was
make, causing  exception been thrown for every file opened. While there,
invert file test existence logic, so we return early on failure.

Fixes: b0c4c10eb23e ("Base: Do not use else before return")

src/Base/Interpreter.cpp
@@ -438,48 +438,49 @@ void InterpreterSingleton::runFile(const char* pxFileName, bool local)
 #else
     FILE* fp = fopen(pxFileName, "r");
 #endif
-    if (fp) {
-        PyGILStateLocker locker;
-        PyObject* module {};
-        PyObject* dict {};
-        module = PyImport_AddModule("__main__");
-        dict = PyModule_GetDict(module);
-        if (local) {
-            dict = PyDict_Copy(dict);
-        }
-        else {
-            Py_INCREF(dict);  // avoid to further distinguish between local and global dict
-        }
-
-        if (!PyDict_GetItemString(dict, "__file__")) {
-            PyObject* pyObj = PyUnicode_FromString(pxFileName);
-            if (!pyObj) {
-                fclose(fp);
-                Py_DECREF(dict);
-                return;
-            }
-            if (PyDict_SetItemString(dict, "__file__", pyObj) < 0) {
-                Py_DECREF(pyObj);
-                fclose(fp);
-                Py_DECREF(dict);
-                return;
-            }
-            Py_DECREF(pyObj);
-        }
-
-        PyObject* result = PyRun_File(fp, pxFileName, Py_file_input, dict, dict);
-        fclose(fp);
-        Py_DECREF(dict);
-
-        if (!result) {
-            if (PyErr_ExceptionMatches(PyExc_SystemExit)) {
-                throw SystemExitException();
-            }
-            throw PyException();
-        }
-        Py_DECREF(result);
+    if (!fp) {
+        throw FileException("Unknown file", pxFileName);
     }
-    throw FileException("Unknown file", pxFileName);
+
+    PyGILStateLocker locker;
+    PyObject* module {};
+    PyObject* dict {};
+    module = PyImport_AddModule("__main__");
+    dict = PyModule_GetDict(module);
+    if (local) {
+        dict = PyDict_Copy(dict);
+    }
+    else {
+        Py_INCREF(dict);  // avoid to further distinguish between local and global dict
+    }
+
+    if (!PyDict_GetItemString(dict, "__file__")) {
+        PyObject* pyObj = PyUnicode_FromString(pxFileName);
+        if (!pyObj) {
+            fclose(fp);
+            Py_DECREF(dict);
+            return;
+        }
+        if (PyDict_SetItemString(dict, "__file__", pyObj) < 0) {
+            Py_DECREF(pyObj);
+            fclose(fp);
+            Py_DECREF(dict);
+            return;
+        }
+        Py_DECREF(pyObj);
+    }
+
+    PyObject* result = PyRun_File(fp, pxFileName, Py_file_input, dict, dict);
+    fclose(fp);
+    Py_DECREF(dict);
+
+    if (!result) {
+        if (PyErr_ExceptionMatches(PyExc_SystemExit)) {
+            throw SystemExitException();
+        }
+        throw PyException();
+    }
+    Py_DECREF(result);
 }
 
 bool InterpreterSingleton::loadModule(const char* psModName)