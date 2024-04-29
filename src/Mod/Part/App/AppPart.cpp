FreeCAD/FreeCAD

[Part] Add handlers for unified measurement facility

src/Mod/Part/App/AppPart.cpp
@@ -184,6 +184,8 @@
 #include <ShapeUpgrade/UnifySameDomainPy.h>
 
 #include <OCAF/ImportExportSettings.h>
+#include "Measure.h"
+
 
 namespace Part {
 extern PyObject* initModule();
@@ -541,10 +543,12 @@ PyMOD_INIT_FUNC(Part)
     Part::Geom2dOffsetCurve       ::init();
     Part::Geom2dTrimmedCurve      ::init();
 
+
     IGESControl_Controller::Init();
     STEPControl_Controller::Init();
 
     OCAF::ImportExportSettings::initialize();
-
+    Part::Measure::initialize();
+    
     PyMOD_Return(partModule);
 }

southseact-3d/fcd

src/Mod/Part/App/AppPart.cpp
@@ -185,6 +185,8 @@
 #include <ShapeUpgrade/UnifySameDomainPy.h>
 
 #include <OCAF/ImportExportSettings.h>
+#include "MeasureClient.h"
+
 
 namespace Part {
 extern PyObject* initModule();
@@ -552,10 +554,12 @@ PyMOD_INIT_FUNC(Part)
     Part::Geom2dOffsetCurve       ::init();
     Part::Geom2dTrimmedCurve      ::init();
 
+
     IGESControl_Controller::Init();
     STEPControl_Controller::Init();
 
     OCAF::ImportExportSettings::initialize();
-
+    Part::MeasureClient::initialize();
+    
     PyMOD_Return(partModule);
 }