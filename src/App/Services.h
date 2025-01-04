FreeCAD/FreeCAD

Gui: Rework TaskCSysDragger into new Transform Dialog
This commit refactor ViewProviderDragger and TaskCSysDragger to be more
modern and to support selecting TransformOrigin.

src/App/Services.h
@@ -1,0 +1,74 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+/****************************************************************************
+ *                                                                          *
+ *   Copyright (c) 2024 Kacper Donat <kacper@kadet.net>                     *
+ *                                                                          *
+ *   This file is part of FreeCAD.                                          *
+ *                                                                          *
+ *   FreeCAD is free software: you can redistribute it and/or modify it     *
+ *   under the terms of the GNU Lesser General Public License as            *
+ *   published by the Free Software Foundation, either version 2.1 of the   *
+ *   License, or (at your option) any later version.                        *
+ *                                                                          *
+ *   FreeCAD is distributed in the hope that it will be useful, but         *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with FreeCAD. If not, see                                *
+ *   <https://www.gnu.org/licenses/>.                                       *
+ *                                                                          *
+ ***************************************************************************/
+
+#ifndef APP_CONTRACTS_H
+#define APP_CONTRACTS_H
+
+#include "DocumentObject.h"
+
+#include <optional>
+#include <Base/Placement.h>
+
+namespace App
+{
+
+/**
+* This service should provide placement of given sub object (like for example face).
+* This feature is not implemented in the core and so it must be provided by module.
+*/
+class SubObjectPlacementProvider
+{
+public:
+    virtual ~SubObjectPlacementProvider() = default;
+
+    /**
+    * Returns placement of sub object relative to the base placement.
+    */
+    virtual Base::Placement calculate(SubObjectT object, Base::Placement basePlacement) const = 0;
+};
+
+/**
+* This service should provide center of mass calculation;
+*/
+class CenterOfMassProvider
+{
+public:
+    virtual ~CenterOfMassProvider() = default;
+
+    virtual std::optional<Base::Vector3d> ofDocumentObject(DocumentObject* object) const = 0;
+};
+
+/**
+* Default implementation for the center of mass contract
+* It always returns empty optional
+*/
+class NullCenterOfMass final : public CenterOfMassProvider
+{
+public:
+    std::optional<Base::Vector3d> ofDocumentObject(DocumentObject* object) const override;
+};
+
+}
+
+
+#endif // APP_CONTRACTS_H

src/Gui/TaskCSysDragger.h
@@ -25,39 +25,138 @@
 #define TASKCSYSDRAGGER_H
 
 #include "TaskView/TaskDialog.h"
-#include <App/DocumentObserver.h>
+#include "TaskView/TaskView.h"
+#include "ViewProviderDragger.h"
+
+#include <Base/ServiceProvider.h>
+#include <App/Services.h>
 
 class SoDragger;
 
-namespace Gui
+namespace Attacher
 {
-  class QuantitySpinBox;
-  class SoFCCSysDragger;
-  class ViewProviderDragger;
-
-  class TaskCSysDragger : public Gui::TaskView::TaskDialog
-  {
-      Q_OBJECT
-    public:
-      TaskCSysDragger(ViewProviderDocumentObject *vpObjectIn, SoFCCSysDragger *draggerIn);
-      ~TaskCSysDragger() override;
-      QDialogButtonBox::StandardButtons getStandardButtons() const override
-        { return QDialogButtonBox::Ok | QDialogButtonBox::Cancel;}
-      void open() override;
-      bool accept() override;
-      bool reject() override;
-    private Q_SLOTS:
-      void onTIncrementSlot(double freshValue);
-      void onRIncrementSlot(double freshValue);
-    private:
-      static inline bool firstDrag = true;
-      static void dragStartCallback(void * data, SoDragger * d);
-      void setupGui();
-      App::DocumentObjectT vpObject;
-      SoFCCSysDragger *dragger;
-      QuantitySpinBox *tSpinBox;
-      QuantitySpinBox *rSpinBox;
-  };
+    class AttachEngine;
 }
 
-#endif // TASKCSYSDRAGGER_H
+namespace Gui
+{
+class QuantitySpinBox;
+class SoFCCSysDragger;
+class ViewProviderDragger;
+class Ui_TaskCSysDragger;
+
+class TaskTransform : public Gui::TaskView::TaskBox, public Gui::SelectionObserver
+{
+    Q_OBJECT
+
+public:
+    enum class SelectionMode { None, SelectTransformOrigin, SelectAlignTarget };
+    enum class PlacementMode { ObjectOrigin, Centroid, Custom };
+    enum class PositionMode { Local, Absolute };
+
+    struct CoordinateSystem
+    {
+        std::array<std::string, 3> labels;
+        Base::Placement origin;
+    };
+
+    Q_ENUM(SelectionMode)
+    Q_ENUM(PlacementMode)
+    Q_ENUM(PositionMode)
+
+    TaskTransform(Gui::ViewProviderDragger* vp,
+                  Gui::SoFCCSysDragger* dragger,
+                  QWidget* parent = nullptr,
+                  App::SubObjectPlacementProvider* subObjectPlacementProvider =
+                      Base::provideImplementation<App::SubObjectPlacementProvider>(),
+                  App::CenterOfMassProvider* centerOfMassProvider =
+                      Base::provideImplementation<App::CenterOfMassProvider>());
+    ~TaskTransform() override;
+
+    void setSelectionMode(SelectionMode mode);
+    SelectionMode getSelectionMode() const;
+
+    CoordinateSystem absoluteCoordinateSystem() const;
+    CoordinateSystem localCoordinateSystem() const;
+    CoordinateSystem currentCoordinateSystem() const;
+
+private:
+    void onSelectionChanged(const SelectionChanges& msg) override;
+
+private Q_SLOTS:
+    void onPlacementModeChange(int index);
+
+    void onPickTransformOrigin();
+    void onTransformOriginReset();
+    void onAlignRotationChanged();
+
+    void onAlignToOtherObject();
+    void onFlip();
+
+    void onCoordinateSystemChange(int mode);
+
+    void onPositionChange();
+    void onRotationChange();
+
+private:
+    static inline bool firstDrag = true;
+    static void dragStartCallback(void* data, SoDragger* d);
+    static void dragMotionCallback(void* data, SoDragger* d);
+
+    void setupGui();
+
+    void loadPreferences();
+    void savePreferences();
+
+    void loadPositionModeItems() const;
+    void loadPlacementModeItems() const;
+
+    void updatePositionAndRotationUi() const;
+    void updateDraggerLabels() const;
+    void updateInputLabels() const;
+    void updateIncrements() const;
+    void updateTransformOrigin();
+
+    bool isDraggerAlignedToCoordinateSystem() const;
+
+    ViewProviderDragger* vp;
+
+    const App::SubObjectPlacementProvider* subObjectPlacementProvider;
+    const App::CenterOfMassProvider *centerOfMassProvider;
+
+    CoinPtr<SoFCCSysDragger> dragger;
+
+    Ui_TaskCSysDragger *ui;
+
+    SelectionMode selectionMode { SelectionMode::None };
+    PlacementMode placementMode { PlacementMode::ObjectOrigin };
+    PositionMode positionMode { PositionMode::Local };
+
+    std::optional<Base::Placement> customTransformOrigin {};
+    Base::Placement originalPlacement {};
+};
+
+class TaskCSysDragger: public Gui::TaskView::TaskDialog
+{
+    Q_OBJECT
+
+public:
+    TaskCSysDragger(ViewProviderDragger* vp, SoFCCSysDragger* dragger);
+    ~TaskCSysDragger() override = default;
+
+    QDialogButtonBox::StandardButtons getStandardButtons() const override
+    {
+        return QDialogButtonBox::Ok | QDialogButtonBox::Cancel;
+    }
+
+    void open() override;
+    bool accept() override;
+    bool reject() override;
+
+private:
+    ViewProviderDragger* vp;
+    TaskTransform* transform;
+};
+}  // namespace Gui
+
+#endif  // TASKCSYSDRAGGER_H

src/Mod/Part/App/Services.h
@@ -1,0 +1,47 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+/****************************************************************************
+ *                                                                          *
+ *   Copyright (c) 2024 Kacper Donat <kacper@kadet.net>                     *
+ *                                                                          *
+ *   This file is part of FreeCAD.                                          *
+ *                                                                          *
+ *   FreeCAD is free software: you can redistribute it and/or modify it     *
+ *   under the terms of the GNU Lesser General Public License as            *
+ *   published by the Free Software Foundation, either version 2.1 of the   *
+ *   License, or (at your option) any later version.                        *
+ *                                                                          *
+ *   FreeCAD is distributed in the hope that it will be useful, but         *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with FreeCAD. If not, see                                *
+ *   <https://www.gnu.org/licenses/>.                                       *
+ *                                                                          *
+ ***************************************************************************/
+
+#ifndef PART_CONTRACTS_H
+#define PART_CONTRACTS_H
+
+#include <Attacher.h>
+#include <App/Services.h>
+
+class AttacherSubObjectPlacement final: public App::SubObjectPlacementProvider
+{
+public:
+    AttacherSubObjectPlacement();
+
+    Base::Placement calculate(App::SubObjectT object, Base::Placement basePlacement) const override;
+
+private:
+    std::unique_ptr<Attacher::AttachEngine3D> attacher;
+};
+
+class PartCenterOfMass final: public App::CenterOfMassProvider
+{
+public:
+    std::optional<Base::Vector3d> ofDocumentObject(App::DocumentObject* object) const override;
+};
+
+#endif  // PART_CONTRACTS_H

southseact-3d/fcd

src/App/Services.h
@@ -1,0 +1,74 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+/****************************************************************************
+ *                                                                          *
+ *   Copyright (c) 2024 Kacper Donat <kacper@kadet.net>                     *
+ *                                                                          *
+ *   This file is part of FreeCAD.                                          *
+ *                                                                          *
+ *   FreeCAD is free software: you can redistribute it and/or modify it     *
+ *   under the terms of the GNU Lesser General Public License as            *
+ *   published by the Free Software Foundation, either version 2.1 of the   *
+ *   License, or (at your option) any later version.                        *
+ *                                                                          *
+ *   FreeCAD is distributed in the hope that it will be useful, but         *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with FreeCAD. If not, see                                *
+ *   <https://www.gnu.org/licenses/>.                                       *
+ *                                                                          *
+ ***************************************************************************/
+
+#ifndef APP_SERVICES_H
+#define APP_SERVICES_H
+
+#include "DocumentObject.h"
+
+#include <optional>
+#include <Base/Placement.h>
+
+namespace App
+{
+
+/**
+* This service should provide placement of given sub object (like for example face).
+* This feature is not implemented in the core and so it must be provided by module.
+*/
+class SubObjectPlacementProvider
+{
+public:
+    virtual ~SubObjectPlacementProvider() = default;
+
+    /**
+    * Returns placement of sub object relative to the base placement.
+    */
+    virtual Base::Placement calculate(SubObjectT object, Base::Placement basePlacement) const = 0;
+};
+
+/**
+* This service should provide center of mass calculation;
+*/
+class CenterOfMassProvider
+{
+public:
+    virtual ~CenterOfMassProvider() = default;
+
+    virtual std::optional<Base::Vector3d> ofDocumentObject(DocumentObject* object) const = 0;
+};
+
+/**
+* Default implementation for the center of mass contract
+* It always returns empty optional
+*/
+class NullCenterOfMass final : public CenterOfMassProvider
+{
+public:
+    std::optional<Base::Vector3d> ofDocumentObject(DocumentObject* object) const override;
+};
+
+}
+
+
+#endif // APP_SERVICES_H

src/Gui/TaskCSysDragger.h
@@ -25,39 +25,138 @@
 #define TASKCSYSDRAGGER_H
 
 #include "TaskView/TaskDialog.h"
-#include <App/DocumentObserver.h>
+#include "TaskView/TaskView.h"
+#include "ViewProviderDragger.h"
+
+#include <Base/ServiceProvider.h>
+#include <App/Services.h>
 
 class SoDragger;
 
-namespace Gui
+namespace Attacher
 {
-  class QuantitySpinBox;
-  class SoFCCSysDragger;
-  class ViewProviderDragger;
-
-  class TaskCSysDragger : public Gui::TaskView::TaskDialog
-  {
-      Q_OBJECT
-    public:
-      TaskCSysDragger(ViewProviderDocumentObject *vpObjectIn, SoFCCSysDragger *draggerIn);
-      ~TaskCSysDragger() override;
-      QDialogButtonBox::StandardButtons getStandardButtons() const override
-        { return QDialogButtonBox::Ok | QDialogButtonBox::Cancel;}
-      void open() override;
-      bool accept() override;
-      bool reject() override;
-    private Q_SLOTS:
-      void onTIncrementSlot(double freshValue);
-      void onRIncrementSlot(double freshValue);
-    private:
-      static inline bool firstDrag = true;
-      static void dragStartCallback(void * data, SoDragger * d);
-      void setupGui();
-      App::DocumentObjectT vpObject;
-      SoFCCSysDragger *dragger;
-      QuantitySpinBox *tSpinBox;
-      QuantitySpinBox *rSpinBox;
-  };
+    class AttachEngine;
 }
 
-#endif // TASKCSYSDRAGGER_H
+namespace Gui
+{
+class QuantitySpinBox;
+class SoFCCSysDragger;
+class ViewProviderDragger;
+class Ui_TaskCSysDragger;
+
+class TaskTransform : public Gui::TaskView::TaskBox, public Gui::SelectionObserver
+{
+    Q_OBJECT
+
+public:
+    enum class SelectionMode { None, SelectTransformOrigin, SelectAlignTarget };
+    enum class PlacementMode { ObjectOrigin, Centroid, Custom };
+    enum class PositionMode { Local, Absolute };
+
+    struct CoordinateSystem
+    {
+        std::array<std::string, 3> labels;
+        Base::Placement origin;
+    };
+
+    Q_ENUM(SelectionMode)
+    Q_ENUM(PlacementMode)
+    Q_ENUM(PositionMode)
+
+    TaskTransform(Gui::ViewProviderDragger* vp,
+                  Gui::SoFCCSysDragger* dragger,
+                  QWidget* parent = nullptr,
+                  App::SubObjectPlacementProvider* subObjectPlacementProvider =
+                      Base::provideService<App::SubObjectPlacementProvider>(),
+                  App::CenterOfMassProvider* centerOfMassProvider =
+                      Base::provideService<App::CenterOfMassProvider>());
+    ~TaskTransform() override;
+
+    void setSelectionMode(SelectionMode mode);
+    SelectionMode getSelectionMode() const;
+
+    CoordinateSystem absoluteCoordinateSystem() const;
+    CoordinateSystem localCoordinateSystem() const;
+    CoordinateSystem currentCoordinateSystem() const;
+
+private:
+    void onSelectionChanged(const SelectionChanges& msg) override;
+
+private Q_SLOTS:
+    void onPlacementModeChange(int index);
+
+    void onPickTransformOrigin();
+    void onTransformOriginReset();
+    void onAlignRotationChanged();
+
+    void onAlignToOtherObject();
+    void onFlip();
+
+    void onCoordinateSystemChange(int mode);
+
+    void onPositionChange();
+    void onRotationChange();
+
+private:
+    static inline bool firstDrag = true;
+    static void dragStartCallback(void* data, SoDragger* d);
+    static void dragMotionCallback(void* data, SoDragger* d);
+
+    void setupGui();
+
+    void loadPreferences();
+    void savePreferences();
+
+    void loadPositionModeItems() const;
+    void loadPlacementModeItems() const;
+
+    void updatePositionAndRotationUi() const;
+    void updateDraggerLabels() const;
+    void updateInputLabels() const;
+    void updateIncrements() const;
+    void updateTransformOrigin();
+
+    bool isDraggerAlignedToCoordinateSystem() const;
+
+    ViewProviderDragger* vp;
+
+    const App::SubObjectPlacementProvider* subObjectPlacementProvider;
+    const App::CenterOfMassProvider *centerOfMassProvider;
+
+    CoinPtr<SoFCCSysDragger> dragger;
+
+    Ui_TaskCSysDragger *ui;
+
+    SelectionMode selectionMode { SelectionMode::None };
+    PlacementMode placementMode { PlacementMode::ObjectOrigin };
+    PositionMode positionMode { PositionMode::Local };
+
+    std::optional<Base::Placement> customTransformOrigin {};
+    Base::Placement originalPlacement {};
+};
+
+class TaskCSysDragger: public Gui::TaskView::TaskDialog
+{
+    Q_OBJECT
+
+public:
+    TaskCSysDragger(ViewProviderDragger* vp, SoFCCSysDragger* dragger);
+    ~TaskCSysDragger() override = default;
+
+    QDialogButtonBox::StandardButtons getStandardButtons() const override
+    {
+        return QDialogButtonBox::Ok | QDialogButtonBox::Cancel;
+    }
+
+    void open() override;
+    bool accept() override;
+    bool reject() override;
+
+private:
+    ViewProviderDragger* vp;
+    TaskTransform* transform;
+};
+}  // namespace Gui
+
+#endif  // TASKCSYSDRAGGER_H

src/Mod/Part/App/Services.h
@@ -1,0 +1,47 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+/****************************************************************************
+ *                                                                          *
+ *   Copyright (c) 2024 Kacper Donat <kacper@kadet.net>                     *
+ *                                                                          *
+ *   This file is part of FreeCAD.                                          *
+ *                                                                          *
+ *   FreeCAD is free software: you can redistribute it and/or modify it     *
+ *   under the terms of the GNU Lesser General Public License as            *
+ *   published by the Free Software Foundation, either version 2.1 of the   *
+ *   License, or (at your option) any later version.                        *
+ *                                                                          *
+ *   FreeCAD is distributed in the hope that it will be useful, but         *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with FreeCAD. If not, see                                *
+ *   <https://www.gnu.org/licenses/>.                                       *
+ *                                                                          *
+ ***************************************************************************/
+
+#ifndef PART_SERVICES_H
+#define PART_SERVICES_H
+
+#include <Attacher.h>
+#include <App/Services.h>
+
+class AttacherSubObjectPlacement final: public App::SubObjectPlacementProvider
+{
+public:
+    AttacherSubObjectPlacement();
+
+    Base::Placement calculate(App::SubObjectT object, Base::Placement basePlacement) const override;
+
+private:
+    std::unique_ptr<Attacher::AttachEngine3D> attacher;
+};
+
+class PartCenterOfMass final: public App::CenterOfMassProvider
+{
+public:
+    std::optional<Base::Vector3d> ofDocumentObject(App::DocumentObject* object) const override;
+};
+
+#endif  // PART_SERVICES_H