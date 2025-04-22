southseact-3d/fcd

commit 24336ea49c736c0ac4f68127f322e027f23ac590

Fem: SolverCalculiX object refactor

src/Mod/Fem/Gui/CMakeLists.txt
@@ -429,6 +429,7 @@ SET(FemGuiPythonUI_SRCS
     Resources/ui/ResultHints.ui
     Resources/ui/ResultShow.ui
     Resources/ui/SolverCalculix.ui
+    Resources/ui/SolverCalculiX.ui
 )
 
 ADD_CUSTOM_TARGET(FemPythonUi ALL

src/Mod/Fem/CMakeLists.txt
@@ -203,6 +203,7 @@ SET(FemObjects_SRCS
     femobjects/mesh_region.py
     femobjects/mesh_result.py
     femobjects/result_mechanical.py
+    femobjects/solver_calculix.py
     femobjects/solver_ccxtools.py
 )
 
@@ -227,6 +228,7 @@ SET(FemSolver_SRCS
 
 SET(FemSolverCalculix_SRCS
     femsolver/calculix/__init__.py
+    femsolver/calculix/calculixtools.py
     femsolver/calculix/solver.py
     femsolver/calculix/tasks.py
     femsolver/calculix/write_constraint_bodyheatsource.py
@@ -603,6 +605,7 @@ SET(FemGuiTaskPanels_SRCS
     femtaskpanels/task_mesh_region.py
     femtaskpanels/task_mesh_netgen.py
     femtaskpanels/task_result_mechanical.py
+    femtaskpanels/task_solver_calculix.py
     femtaskpanels/task_solver_ccxtools.py
 )
 
@@ -652,6 +655,7 @@ SET(FemGuiViewProvider_SRCS
     femviewprovider/view_mesh_region.py
     femviewprovider/view_mesh_result.py
     femviewprovider/view_result_mechanical.py
+    femviewprovider/view_solver_calculix.py
     femviewprovider/view_solver_ccxtools.py
 )
 

src/Mod/Fem/Gui/Resources/ui/SolverCalculiX.ui
@@ -1,0 +1,144 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>SolverCalculiX</class>
+ <widget class="QWidget" name="SolverCalculiX">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>400</width>
+    <height>475</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>Solver CalculiX Control</string>
+  </property>
+  <layout class="QVBoxLayout" name="verticalLayout">
+   <item>
+    <widget class="QCheckBox" name="ckb_working_directory">
+     <property name="text">
+      <string>Working Directory</string>
+     </property>
+    </widget>
+   </item>
+   <item>
+    <widget class="QGroupBox" name="gpb_working_directory">
+     <property name="title">
+      <string></string>
+     </property>
+     <layout class="QVBoxLayout" name="vbl_working_directory">
+      <item>
+       <layout class="QHBoxLayout" name="hbl_working_directory">
+        <item>
+         <widget class="QPushButton" name="pb_write_input">
+          <property name="text">
+           <string>Write</string>
+          </property>
+         </widget>
+        </item>
+        <item>
+         <widget class="QPushButton" name="pb_edit_input">
+          <property name="enabled">
+           <bool>false</bool>
+          </property>
+          <property name="text">
+           <string>Edit</string>
+          </property>
+         </widget>
+        </item>
+       </layout>
+      </item>
+      <item>
+       <layout class="QHBoxLayout" name="hbl_working_directory">
+        <item>
+         <widget class="QLineEdit" name="let_working_directory">
+          <property name="toolTip">
+           <string>Path to working directory</string>
+          </property>
+          <property name="readOnly">
+           <bool>true</bool>
+          </property>
+         </widget>
+        </item>
+        <item>
+         <widget class="QToolButton" name="pb_working_directory">
+          <property name="text">
+           <string>...</string>
+          </property>
+         </widget>
+        </item>
+       </layout>
+      </item>
+     </layout>
+    </widget>
+   </item>
+   <item>
+    <widget class="QGroupBox" name="gpb_solver_params">
+     <property name="title">
+      <string>Solver Parameters</string>
+     </property>
+     <layout class="QVBoxLayout" name="verticalLayout_9">
+      <item>
+       <layout class="QFormLayout" name="formLayout_1">
+        <item row="0" column="0">
+         <widget class="QLabel" name="lbl_analysis_type">
+          <property name="text">
+           <string>Analysis Type:</string>
+          </property>
+         </widget>
+        </item>
+        <item row="0" column="1">
+         <widget class="QComboBox" name="cb_analysis_type">
+          <property name="sizePolicy">
+           <sizepolicy hsizetype="Expanding" vsizetype="Minimum">
+            <horstretch>0</horstretch>
+            <verstretch>0</verstretch>
+           </sizepolicy>
+          </property>
+         </widget>
+        </item>
+       </layout>
+      </item>
+     </layout>
+    </widget>
+   </item>
+   <item>
+    <widget class="QGroupBox" name="gb_solver_log">
+     <layout class="QVBoxLayout" name="verticalLayout_8">
+      <item>
+       <widget class="QTextEdit" name="te_output">
+        <property name="lineWrapMode">
+         <enum>QTextEdit::NoWrap</enum>
+        </property>
+        <property name="readOnly">
+         <bool>true</bool>
+        </property>
+       </widget>
+      </item>
+      <item>
+       <widget class="QLabel" name="l_time">
+        <property name="font">
+         <font>
+          <pointsize>12</pointsize>
+         </font>
+        </property>
+        <property name="text">
+         <string>Time:</string>
+        </property>
+       </widget>
+      </item>
+      <item>
+       <widget class="QPushButton" name="pb_solver_version">
+        <property name="text">
+         <string>Solver Version</string>
+        </property>
+       </widget>
+      </item>
+     </layout>
+    </widget>
+   </item>
+  </layout>
+ </widget>
+ <resources/>
+ <connections/>
+</ui>

src/Mod/Fem/ObjectsFem.py
@@ -795,12 +795,17 @@ def makeSolverCalculiXCcxTools(doc, name="SolverCcxTools"):
     return obj
 
 
-def makeSolverCalculix(doc, name="SolverCalculix"):
-    """makeSolverCalculix(document, [name]):
+def makeSolverCalculiX(doc, name="SolverCalculiX"):
+    """makeSolverCalculiX(document, [name]):
     makes a Calculix solver object"""
-    import femsolver.calculix.solver
+    obj = doc.addObject("Fem::FemSolverObjectPython", name)
+    from femobjects import solver_calculix
 
-    obj = femsolver.calculix.solver.create(doc, name)
+    solver_calculix.SolverCalculiX(obj)
+    if FreeCAD.GuiUp:
+        from femviewprovider import view_solver_calculix
+
+        view_solver_calculix.VPSolverCalculiX(obj.ViewObject)
     return obj
 
 

src/Mod/Fem/femcommands/commands.py
@@ -630,7 +630,7 @@ class _MaterialMechanicalNonlinear(CommandManager):
         # CalculiX solver or new frame work CalculiX solver
         if solver_object and (
             is_of_type(solver_object, "Fem::SolverCcxTools")
-            or is_of_type(solver_object, "Fem::SolverCalculix")
+            or is_of_type(solver_object, "Fem::SolverCalculiX")
         ):
             FreeCAD.Console.PrintMessage(
                 f"Set MaterialNonlinearity to nonlinear for {solver_object.Label}\n"
@@ -938,7 +938,7 @@ class _SolverCalculixContextManager:
 
     def __enter__(self):
         ccx_prefs = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Fem/Ccx")
-        FreeCAD.ActiveDocument.openTransaction("Create SolverCalculix")
+        FreeCAD.ActiveDocument.openTransaction("Create SolverCalculiX")
         FreeCADGui.addModule("ObjectsFem")
         FreeCADGui.addModule("FemGui")
         FreeCADGui.doCommand(
@@ -1051,8 +1051,8 @@ class _SolverCcxTools(CommandManager):
                 FreeCADGui.doCommand(f"{cm.cli_name}.MaterialNonlinearity = 'nonlinear'")
 
 
-class _SolverCalculix(CommandManager):
-    "The FEM_SolverCalculix command definition"
+class _SolverCalculiX(CommandManager):
+    "The FEM_SolverCalculiX command definition"
 
     def __init__(self):
         super().__init__()
@@ -1068,7 +1068,13 @@ class _SolverCalculix(CommandManager):
         self.is_active = "with_analysis"
 
     def Activated(self):
-        with _SolverCalculixContextManager("makeSolverCalculix", "solver") as cm:
+        ccx_prefs = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Fem/Ccx")
+        if ccx_prefs.GetBool("ResultAsPipeline", False):
+            make_solver = "makeSolverCalculiX"
+        else:
+            make_solver = "makeSolverCalculiXCcxTools"
+
+        with _SolverCalculixContextManager(make_solver, "solver") as cm:
             has_nonlinear_material_obj = False
             for m in self.active_analysis.Group:
                 if is_of_type(m, "Fem::MaterialMechanicalNonlinear"):
@@ -1227,7 +1233,7 @@ FreeCADGui.addCommand("FEM_MeshRegion", _MeshRegion())
 FreeCADGui.addCommand("FEM_ResultShow", _ResultShow())
 FreeCADGui.addCommand("FEM_ResultsPurge", _ResultsPurge())
 FreeCADGui.addCommand("FEM_SolverCalculiXCcxTools", _SolverCcxTools())
-FreeCADGui.addCommand("FEM_SolverCalculiX", _SolverCalculix())
+FreeCADGui.addCommand("FEM_SolverCalculiX", _SolverCalculiX())
 FreeCADGui.addCommand("FEM_SolverControl", _SolverControl())
 FreeCADGui.addCommand("FEM_SolverElmer", _SolverElmer())
 FreeCADGui.addCommand("FEM_SolverMystran", _SolverMystran())

src/Mod/Fem/femmesh/meshsetsgetter.py
@@ -74,7 +74,7 @@ class MeshSetsGetter:
                 # TODO somehow this is not smart, pur mesh objects might be used often
                 if self.member.geos_beamsection and (
                     type_of_obj(self.solver_obj) == "Fem::SolverCcxTools"
-                    or type_of_obj(self.solver_obj) == "Fem::SolverCalculix"
+                    or type_of_obj(self.solver_obj) == "Fem::SolverCalculiX"
                 ):
                     FreeCAD.Console.PrintError(
                         "The mesh does not know the geometry it is made from. "

src/Mod/Fem/femobjects/solver_calculix.py
@@ -1,0 +1,46 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+# ***************************************************************************
+# *   Copyright (c) 2017 Bernd Hahnebach <bernd@bimstatik.org>              *
+# *   Copyright (c) 2025 Mario Passaglia <mpassaglia[at]cbc.uba.ar>         *
+# *                                                                         *
+# *   This file is part of FreeCAD.                                         *
+# *                                                                         *
+# *   FreeCAD is free software: you can redistribute it and/or modify it    *
+# *   under the terms of the GNU Lesser General Public License as           *
+# *   published by the Free Software Foundation, either version 2.1 of the  *
+# *   License, or (at your option) any later version.                       *
+# *                                                                         *
+# *   FreeCAD is distributed in the hope that it will be useful, but        *
+# *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+# *   Lesser General Public License for more details.                       *
+# *                                                                         *
+# *   You should have received a copy of the GNU Lesser General Public      *
+# *   License along with FreeCAD. If not, see                               *
+# *   <https://www.gnu.org/licenses/>.                                      *
+# *                                                                         *
+# ***************************************************************************
+
+__title__ = "FreeCAD FEM solver CalculiX document object"
+__author__ = "Bernd Hahnebach, Mario Passaglia"
+__url__ = "https://www.freecad.org"
+
+## @package solver_calculix
+#  \ingroup FEM
+#  \brief solver CalculiX object
+
+from . import base_fempythonobject
+from femsolver.calculix.solver import _BaseSolverCalculix
+
+
+class SolverCalculiX(base_fempythonobject.BaseFemPythonObject, _BaseSolverCalculix):
+
+    Type = "Fem::SolverCalculiX"
+
+    def __init__(self, obj):
+        super().__init__(obj)
+        self.add_attributes(obj)
+
+    def onDocumentRestored(self, obj):
+        self.on_restore_of_document(obj)

src/Mod/Fem/femsolver/calculix/calculixtools.py
@@ -1,0 +1,190 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+# ***************************************************************************
+# *   Copyright (c) 2025 Mario Passaglia <mpassaglia[at]cbc.uba.ar>         *
+# *                                                                         *
+# *   This file is part of FreeCAD.                                         *
+# *                                                                         *
+# *   FreeCAD is free software: you can redistribute it and/or modify it    *
+# *   under the terms of the GNU Lesser General Public License as           *
+# *   published by the Free Software Foundation, either version 2.1 of the  *
+# *   License, or (at your option) any later version.                       *
+# *                                                                         *
+# *   FreeCAD is distributed in the hope that it will be useful, but        *
+# *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
+# *   Lesser General Public License for more details.                       *
+# *                                                                         *
+# *   You should have received a copy of the GNU Lesser General Public      *
+# *   License along with FreeCAD. If not, see                               *
+# *   <https://www.gnu.org/licenses/>.                                      *
+# *                                                                         *
+# ***************************************************************************
+
+__title__ = "Tools for the work with CalculiX solver"
+__author__ = "Mario Passaglia"
+__url__ = "https://www.freecad.org"
+
+
+from PySide.QtCore import QProcess, QThread
+import tempfile
+import os
+import shutil
+
+import FreeCAD
+import Fem
+
+from . import writer
+from .. import settings
+
+# from feminout import importCcxDatResults
+from femmesh import meshsetsgetter
+from femtools import membertools
+
+
+class CalculiXTools:
+
+    frd_var_conversion = {
+        "CONTACT": "Contact Displacement",
+        "PE": "Plastic Strain",
+        "CELS": "Contact Energy",
+        "ECD": "Current Density",
+        "EMFB": "Magnetic Field",
+        "EMFE": "Electric Field",
+        "ENER": "Internal Energy Density",
+        "FLUX": "Heat Flux",
+        "DISP": "Displacement",
+        "T": "Temperature",
+        "TOSTRAIN": "Strain",
+        "STRESS": "Stress",
+        "STR(%)": "Error",
+    }
+
+    name = "CalculiX"
+
+    def __init__(self, obj):
+        self.obj = obj
+        self.process = QProcess()
+        self.model_file = ""
+        self.analysis = obj.getParentGroup()
+        self.fem_param = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Fem")
+        self._create_working_directory(obj)
+
+    def _create_working_directory(self, obj):
+        """
+        Create working directory according to preferences
+        """
+        if not os.path.isdir(obj.WorkingDirectory):
+            gen_param = self.fem_param.GetGroup("General")
+            if gen_param.GetBool("UseTempDirectory"):
+                self.obj.WorkingDirectory = tempfile.mkdtemp(prefix="fem_")
+            elif gen_param.GetBool("UseBesideDirectory"):
+                root, ext = os.path.splitext(obj.Document.FileName)
+                if root:
+                    self.obj.WorkingDirectory = os.path.join(root, obj.Label)
+                    os.makedirs(self.obj.WorkingDirectory, exist_ok=True)
+                else:
+                    # file not saved, use temporary
+                    self.obj.WorkingDirectory = tempfile.mkdtemp(prefix="fem_")
+            elif gen_param.GetBool("UseCustomDirectory"):
+                self.obj.WorkingDirectory = gen_param.GetString("CustomDirectoryPath")
+                os.makedirs(self.obj.WorkingDirectory, exist_ok=True)
+
+    def prepare(self):
+        from femtools.checksanalysis import check_member_for_solver_calculix
+
+        self._clear_results()
+
+        message = check_member_for_solver_calculix(
+            self.analysis,
+            self.obj,
+            membertools.get_mesh_to_solve(self.analysis)[0],
+            membertools.AnalysisMember(self.analysis),
+        )
+
+        mesh_obj = membertools.get_mesh_to_solve(self.analysis)[0]
+        meshdatagetter = meshsetsgetter.MeshSetsGetter(
+            self.analysis,
+            self.obj,
+            mesh_obj,
+            membertools.AnalysisMember(self.analysis),
+        )
+        meshdatagetter.get_mesh_sets()
+
+        # write solver input
+        w = writer.FemInputWriterCcx(
+            self.analysis,
+            self.obj,
+            mesh_obj,
+            meshdatagetter.member,
+            self.obj.WorkingDirectory,
+            meshdatagetter.mat_geo_sets,
+        )
+        self.model_file = w.write_solver_input()
+        # report to user if task succeeded
+        self.input_deck = os.path.splitext(os.path.basename(self.model_file))[0]
+
+    def compute(self):
+        self._clear_results()
+        ccx_bin = settings.get_binary("Calculix")
+        env = self.process.processEnvironment()
+        num_cpu = self.fem_param.GetGroup("Ccx").GetInt(
+            "AnalysisNumCPUs", QThread.idealThreadCount()
+        )
+        env.insert("OMP_NUM_THREADS", str(num_cpu))
+        self.process.setProcessEnvironment(env)
+        self.process.setWorkingDirectory(self.obj.WorkingDirectory)
+
+        command_list = ["-i", os.path.join(self.obj.WorkingDirectory, self.input_deck)]
+        self.process.start(ccx_bin, command_list)
+
+        return self.process
+
+    def update_properties(self):
+        # TODO at the moment, only one .vtm file is assumed
+        if not self.obj.Results:
+            pipeline = self.obj.Document.addObject("Fem::FemPostPipeline", self.obj.Name + "Result")
+            self.analysis.addObject(pipeline)
+            self.obj.Results = [pipeline]
+            self._load_ccxfrd_results()
+            # default display mode
+            pipeline.ViewObject.DisplayMode = "Surface"
+            pipeline.ViewObject.SelectionStyle = "BoundBox"
+            if self.obj.AnalysisType in ["static", "frequency", "buckling"]:
+                pipeline.ViewObject.Field = "Displacement"
+            elif self.obj.AnalysisType in ["thermomech"]:
+                pipeline.ViewObject.Field = "Temperature"
+        else:
+            self._load_ccxfrd_results()
+
+    def _clear_results(self):
+        # result is a 'Result.vtm' file and a 'Result' directory
+        # with the .vtu files
+        dir_content = os.listdir(self.obj.WorkingDirectory)
+        for f in dir_content:
+            path = os.path.join(self.obj.WorkingDirectory, f)
+            base, ext = os.path.splitext(path)
+            if ext == ".vtm":
+                # remove .vtm file
+                os.remove(path)
+                # remove dir with .vtu files
+                shutil.rmtree(base)
+
+    def _load_ccxfrd_results(self):
+        frd_result_prefix = os.path.join(self.obj.WorkingDirectory, self.input_deck)
+        Fem.frdToVTK(frd_result_prefix + ".frd")
+        files = os.listdir(self.obj.WorkingDirectory)
+        for f in files:
+            if f.endswith(".vtm"):
+                res = os.path.join(self.obj.WorkingDirectory, f)
+                self.obj.Results[0].read(res)
+                self.obj.Results[0].renameArrays(self.frd_var_conversion)
+                break
+
+    def version(self):
+        p = QProcess()
+        ccx_bin = settings.get_binary("Calculix")
+        p.start(ccx_bin, ["-v"])
+        p.waitForFinished()
+        info = p.readAll().data().decode()
+        return info

commit cd78db63ae4c8e026678896f5cefc0cd5bd63759

Fix SolverCalculiX object refactor (#20913)
Rename original SolverCalculix.ui to SolverCcxTools.ui to prevent
collision on case insensitive filesystems.

Fixes: 24336ea49c73 ("Fem: SolverCalculiX object refactor")

src/Mod/Fem/Gui/CMakeLists.txt
@@ -428,8 +428,8 @@ SET(FemGuiPythonUI_SRCS
     Resources/ui/MeshRegion.ui
     Resources/ui/ResultHints.ui
     Resources/ui/ResultShow.ui
-    Resources/ui/SolverCalculix.ui
     Resources/ui/SolverCalculiX.ui
+    Resources/ui/SolverCcxTools.ui
 )
 
 ADD_CUSTOM_TARGET(FemPythonUi ALL

src/Mod/Fem/Gui/Resources/Fem.qrc
@@ -148,6 +148,7 @@
         <file>ui/MeshRegion.ui</file>
         <file>ui/ResultHints.ui</file>
         <file>ui/ResultShow.ui</file>
-        <file>ui/SolverCalculix.ui</file>
+        <file>ui/SolverCalculiX.ui</file>
+        <file>ui/SolverCcxTools.ui</file>
     </qresource>
 </RCC>

src/Mod/Fem/femtaskpanels/task_solver_ccxtools.py
@@ -56,7 +56,7 @@ class _TaskPanel:
 
     def __init__(self, solver_object):
         self.form = FreeCADGui.PySideUic.loadUi(
-            FreeCAD.getHomePath() + "Mod/Fem/Resources/ui/SolverCalculix.ui"
+            FreeCAD.getHomePath() + "Mod/Fem/Resources/ui/SolverCcxTools.ui"
         )
 
         from femtools.ccxtools import CcxTools as ccx