southseact-3d/fcd

commit baec0f8cee94bb897dfd18843d9f282b59657d78

Base: simplify UnitsSchemas management
Fixes: Maintaining schemas is difficult and error-prone

- Facilitate easy schemas add, remove, change, etc.
- Remove 14 files containing approx 2,190 lines of if/else code and data
- Place data in one file (UnitsSchemasData.h) using a normalized structure (including special functions)
- Isolate and simplify data operations (code)
- Remove schemas enum to keep data independent of code
- Separate responsibilities: Specifications, data, schemas, schema
- Add schema data 'isDefault'
- Add schema data name
- Prefer algorithms to raw loops
- Add schemas unit tests
- Tweak quantity unit tests

src/Base/UnitsApiPy.cpp
@@ -20,9 +20,7 @@
  *                                                                         *
  ***************************************************************************/
 
-
 #include "PreCompiled.h"
-
 #ifndef _PreComp_
 #include <memory>
 #endif
@@ -39,10 +37,9 @@ using namespace Base;
 //**************************************************************************
 // Python stuff of UnitsApi
 
-// UnitsApi Methods
 PyMethodDef UnitsApi::Methods[] = {
     {"parseQuantity",
-     UnitsApi::sParseQuantity,
+     sParseQuantity,
      METH_VARARGS,
      "parseQuantity(string) -> Base.Quantity()\n\n"
      "calculate a mathematical expression with units to a quantity object. \n"
@@ -51,27 +48,27 @@ PyMethodDef UnitsApi::Methods[] = {
      "or for more complex espressions:\n"
      "parseQuantity('sin(pi)/50.0 m/s^2')\n"},
     {"listSchemas",
-     UnitsApi::sListSchemas,
+     sListSchemas,
      METH_VARARGS,
      "listSchemas() -> a tuple of schemas\n\n"
      "listSchemas(int) -> description of the given schema\n\n"},
     {"getSchema",
-     UnitsApi::sGetSchema,
+     sGetSchema,
      METH_VARARGS,
      "getSchema() -> int\n\n"
      "The int is the position of the tuple returned by listSchemas"},
     {"setSchema",
-     UnitsApi::sSetSchema,
+     sSetSchema,
      METH_VARARGS,
      "setSchema(int) -> None\n\n"
      "Sets the current schema to the given number, if possible"},
     {"schemaTranslate",
-     UnitsApi::sSchemaTranslate,
+     sSchemaTranslate,
      METH_VARARGS,
      "schemaTranslate(Quantity, int) -> tuple\n\n"
      "Translate a quantity to a given schema"},
     {"toNumber",
-     UnitsApi::sToNumber,
+     sToNumber,
      METH_VARARGS,
      "toNumber(Quantity or float, [format='g', decimals=-1]) -> str\n\n"
      "Convert a quantity or float to a string"},
@@ -86,30 +83,32 @@ PyObject* UnitsApi::sParseQuantity(PyObject* /*self*/, PyObject* args)
         return nullptr;
     }
 
-    Quantity rtn;
-    std::string str(pstr);
+    const std::string str {pstr};
     PyMem_Free(pstr);
     try {
-        rtn = Quantity::parse(str);
+        return new QuantityPy(new Quantity(Quantity::parse(str)));
     }
-    catch (const Base::ParserError&) {
-        PyErr_Format(PyExc_ValueError, "invalid unit expression \n");
+    catch (const ParserError&) {
+        PyErr_Format(PyExc_ValueError,
+                     "invalid unit expression: '%s'\n",
+                     std::string {pstr}.c_str());
         return nullptr;
     }
-
-    return new QuantityPy(new Quantity(rtn));
 }
 
 PyObject* UnitsApi::sListSchemas(PyObject* /*self*/, PyObject* args)
 {
+    auto names = UnitsApi::getNames();
+    const int num = static_cast<int>(names.size());
+
     if (PyArg_ParseTuple(args, "")) {
-        int num = static_cast<int>(UnitSystem::NumUnitSystemTypes);
-        Py::Tuple tuple(num);
-        for (int i = 0; i < num; i++) {
-            const auto description {
-                UnitsApi::getDescription(static_cast<UnitSystem>(i)).toStdString()};
-            tuple.setItem(i, Py::String(description.c_str()));
-        }
+        Py::Tuple tuple {num};
+
+        auto addItem = [&, i {0}](const std::string& name) mutable {
+            tuple.setItem(i++, Py::String {name.c_str()});
+        };
+
+        std::for_each(names.begin(), names.end(), addItem);
 
         return Py::new_reference_to(tuple);
     }
@@ -117,14 +116,12 @@ PyObject* UnitsApi::sListSchemas(PyObject* /*self*/, PyObject* args)
     PyErr_Clear();
     int index {};
     if (PyArg_ParseTuple(args, "i", &index)) {
-        int num = static_cast<int>(UnitSystem::NumUnitSystemTypes);
         if (index < 0 || index >= num) {
             PyErr_SetString(PyExc_ValueError, "invalid schema value");
             return nullptr;
         }
 
-        const auto description {
-            UnitsApi::getDescription(static_cast<UnitSystem>(index)).toStdString()};
+        const auto description = schemas->descriptions().at(index);
         return Py_BuildValue("s", description.c_str());
     }
 
@@ -138,20 +135,21 @@ PyObject* UnitsApi::sGetSchema(PyObject* /*self*/, PyObject* args)
         return nullptr;
     }
 
-    return Py_BuildValue("i", static_cast<int>(currentSystem));
+    return Py_BuildValue("i", count());
 }
 
 PyObject* UnitsApi::sSetSchema(PyObject* /*self*/, PyObject* args)
 {
     PyErr_Clear();
     int index {};
-    if (PyArg_ParseTuple(args, "i", &index)) {
-        int num = static_cast<int>(UnitSystem::NumUnitSystemTypes);
-        if (index < 0 || index >= num) {
+    if (PyArg_ParseTuple(args, "i", &index) != 0) {
+
+        if (index < 0 || index >= static_cast<int>(count())) {
             PyErr_SetString(PyExc_ValueError, "invalid schema value");
             return nullptr;
         }
-        setSchema(static_cast<UnitSystem>(index));
+
+        schemas->select(index);
     }
     Py_Return;
 }
@@ -160,27 +158,26 @@ PyObject* UnitsApi::sSchemaTranslate(PyObject* /*self*/, PyObject* args)
 {
     PyObject* py {};
     int index {};
-    if (!PyArg_ParseTuple(args, "O!i", &(QuantityPy::Type), &py, &index)) {
+    if (!PyArg_ParseTuple(args, "O!i", &QuantityPy::Type, &py, &index)) {
         return nullptr;
     }
 
-    Quantity quant;
-    quant = *static_cast<Base::QuantityPy*>(py)->getQuantityPtr();
-
-    std::unique_ptr<UnitsSchema> schema(createSchema(static_cast<UnitSystem>(index)));
-    if (!schema) {
-        PyErr_SetString(PyExc_ValueError, "invalid schema value");
+    if (index < 0 || index >= static_cast<int>(count())) {
+        PyErr_SetString(PyExc_ValueError,
+                        std::string {"invalid schema index:" + std::to_string(index)}.c_str());
         return nullptr;
     }
+
+    const Quantity quant {*static_cast<QuantityPy*>(py)->getQuantityPtr()};
 
     double factor {};
-    std::string uus;
-    std::string uss = schema->schemaTranslate(quant, factor, uus);
+    std::string unitStr;
+    const std::string unitStrLocalised = schemaTranslate(quant, factor, unitStr);
 
-    Py::Tuple res(3);
-    res[0] = Py::String(uss, "utf-8");
-    res[1] = Py::Float(factor);
-    res[2] = Py::String(uus, "utf-8");
+    Py::Tuple res {3};
+    res[0] = Py::String {unitStrLocalised, "utf-8"};
+    res[1] = Py::Float {factor};
+    res[2] = Py::String {unitStr, "utf-8"};
 
     return Py::new_reference_to(res);
 }

src/App/Application.cpp
@@ -2795,12 +2795,10 @@ void Application::initApplication()
     // set up Unit system default
     const ParameterGrp::handle hGrp = GetApplication().GetParameterGroupByPath
        ("User parameter:BaseApp/Preferences/Units");
-    Base::UnitsApi::setSchema(static_cast<Base::UnitSystem>(hGrp->GetInt("UserSchema", 0)));
-    Base::UnitsApi::setDecimals(static_cast<int>(hGrp->GetInt("Decimals", Base::UnitsApi::getDecimals())));
-
-    // In case we are using fractional inches, get user setting for min unit
-    const int denom = static_cast<int>(hGrp->GetInt("FracInch", Base::QuantityFormat::getDefaultDenominator()));
-    Base::QuantityFormat::setDefaultDenominator(denom);
+    Base::UnitsApi::setSchema(hGrp->GetInt("UserSchema", Base::UnitsApi::getDefSchemaNum()));
+    Base::UnitsApi::setDecimals(hGrp->GetInt("Decimals", Base::UnitsApi::getDecimals()));
+    Base::QuantityFormat::setDefaultDenominator(
+        hGrp->GetInt("FracInch", Base::QuantityFormat::getDefaultDenominator()));
 
 #if defined (_DEBUG)
     Base::Console().Log("Application is built with debug information\n");

src/App/Document.cpp
@@ -876,13 +876,8 @@ Document::Document(const char* documentName)
                       "Additional tag to save the name of the company");
     ADD_PROPERTY_TYPE(UnitSystem, (""), 0, Prop_None, "Unit system to use in this project");
     // Set up the possible enum values for the unit system
-    int num = static_cast<int>(Base::UnitSystem::NumUnitSystemTypes);
-    std::vector<std::string> enumValsAsVector;
-    for (int i = 0; i < num; i++) {
-        QString item = Base::UnitsApi::getDescription(static_cast<Base::UnitSystem>(i));
-        enumValsAsVector.emplace_back(item.toStdString());
-    }
-    UnitSystem.setEnums(enumValsAsVector);
+
+    UnitSystem.setEnums(Base::UnitsApi::getDescriptions());
     // Get the preferences/General unit system as the default for a new document
     ParameterGrp::handle hGrpu =
         App::GetApplication().GetParameterGroupByPath("User parameter:BaseApp/Preferences/Units");

src/Base/CMakeLists.txt
@@ -150,20 +150,10 @@ SET(FreeCADBase_UNITAPI_SRCS
     UnitsApi.h
     UnitsSchema.h
     UnitsSchema.cpp
-    UnitsSchemaInternal.h
-    UnitsSchemaInternal.cpp
-    UnitsSchemaMKS.h
-    UnitsSchemaMKS.cpp
-    UnitsSchemaImperial1.h
-    UnitsSchemaImperial1.cpp
-    UnitsSchemaCentimeters.h
-    UnitsSchemaCentimeters.cpp
-    UnitsSchemaMmMin.h
-    UnitsSchemaMmMin.cpp
-    UnitsSchemaFemMilliMeterNewton.h
-    UnitsSchemaFemMilliMeterNewton.cpp
-    UnitsSchemaMeterDecimal.h
-    UnitsSchemaMeterDecimal.cpp
+    UnitsSchemas.cpp
+    UnitsSchemas.h
+    UnitsSchemasData.h
+    UnitsSchemasSpecs.h
     Quantity.h
     Quantity.cpp
     QuantityPyImp.cpp

src/Base/Quantity.cpp
@@ -64,7 +64,7 @@ int QuantityFormat::defaultDenominator = 8;  // for 1/8"
 QuantityFormat::QuantityFormat()
     : option(OmitGroupSeparator | RejectGroupSeparator)
     , format(Fixed)
-    , precision(UnitsApi::getDecimals())
+    , precision(static_cast<int>(UnitsApi::getDecimals()))
     , denominator(defaultDenominator)
 {}
 
@@ -256,14 +256,15 @@ std::string Quantity::getUserString(double& factor, std::string& unitString) con
 std::string
 Quantity::getUserString(UnitsSchema* schema, double& factor, std::string& unitString) const
 {
-    return schema->schemaTranslate(*this, factor, unitString);
+    return schema->translate(*this, factor, unitString);
 }
 
 std::string Quantity::getSafeUserString() const
 {
     auto userStr = getUserString();
     if (myValue != 0.0 && parse(userStr).getValue() == 0) {
-        userStr = fmt::format("{} {}", myValue, getUnit().getString());
+        auto unitStr = getUnit().getString();
+        userStr = fmt::format("{}{}{}", myValue, unitStr.empty() ? "" : " ", unitStr);
     }
 
     return Tools::escapeQuotesFromString(userStr);

src/Base/UnitsApi.cpp
@@ -20,124 +20,91 @@
  *                                                                         *
  ***************************************************************************/
 
-
 #include "PreCompiled.h"
-
-#include <sstream>
+#ifndef _PreComp_
 #include <iomanip>
-#include <memory>
+#include <sstream>
+#endif
 
 #include <CXX/WrapPython.h>
 #include <fmt/format.h>
-#include <QString>
+
 #include "Exception.h"
-
 #include "UnitsApi.h"
-#include "UnitsSchemaCentimeters.h"
-#include "UnitsSchemaInternal.h"
-#include "UnitsSchemaImperial1.h"
-#include "UnitsSchemaMKS.h"
-#include "UnitsSchemaMmMin.h"
-#include "UnitsSchemaFemMilliMeterNewton.h"
-#include "UnitsSchemaMeterDecimal.h"
+#include "UnitsSchema.h"
+#include "UnitsSchemas.h"
+#include "UnitsSchemasData.h"
 
-using namespace Base;
+using Base::UnitsApi;
+using Base::UnitsSchema;
+using Base::UnitsSchemas;
 
-// === static attributes  ================================================
-
-UnitsSchemaPtr UnitsApi::UserPrefSystem(new UnitsSchemaInternal());
-UnitSystem UnitsApi::currentSystem = UnitSystem::SI1;
-
-int UnitsApi::UserPrefDecimals = 2;
-
-QString UnitsApi::getDescription(UnitSystem system)
+void UnitsApi::init()
 {
-    switch (system) {
-        case UnitSystem::SI1:
-            return tr("Standard (mm, kg, s, °)");
-        case UnitSystem::SI2:
-            return tr("MKS (m, kg, s, °)");
-        case UnitSystem::Imperial1:
-            return tr("US customary (in, lb)");
-        case UnitSystem::ImperialDecimal:
-            return tr("Imperial decimal (in, lb)");
-        case UnitSystem::Centimeters:
-            return tr("Building Euro (cm, m², m³)");
-        case UnitSystem::ImperialBuilding:
-            return tr("Building US (ft-in, sqft, cft)");
-        case UnitSystem::MmMin:
-            return tr("Metric small parts & CNC (mm, mm/min)");
-        case UnitSystem::ImperialCivil:
-            return tr("Imperial for Civil Eng (ft, ft/s)");
-        case UnitSystem::FemMilliMeterNewton:
-            return tr("FEM (mm, N, s)");
-        case UnitSystem::MeterDecimal:
-            return tr("Meter decimal (m, m², m³)");
-        default:
-            return tr("Unknown schema");
-    }
+    schemas = std::make_unique<UnitsSchemas>(UnitsSchemasData::unitSchemasDataPack);
 }
 
-UnitsSchemaPtr UnitsApi::createSchema(UnitSystem system)
+std::vector<std::string> UnitsApi::getDescriptions()
 {
-    switch (system) {
-        case UnitSystem::SI1:
-            return std::make_unique<UnitsSchemaInternal>();
-        case UnitSystem::SI2:
-            return std::make_unique<UnitsSchemaMKS>();
-        case UnitSystem::Imperial1:
-            return std::make_unique<UnitsSchemaImperial1>();
-        case UnitSystem::ImperialDecimal:
-            return std::make_unique<UnitsSchemaImperialDecimal>();
-        case UnitSystem::Centimeters:
-            return std::make_unique<UnitsSchemaCentimeters>();
-        case UnitSystem::ImperialBuilding:
-            return std::make_unique<UnitsSchemaImperialBuilding>();
-        case UnitSystem::MmMin:
-            return std::make_unique<UnitsSchemaMmMin>();
-        case UnitSystem::ImperialCivil:
-            return std::make_unique<UnitsSchemaImperialCivil>();
-        case UnitSystem::FemMilliMeterNewton:
-            return std::make_unique<UnitsSchemaFemMilliMeterNewton>();
-        case UnitSystem::MeterDecimal:
-            return std::make_unique<UnitsSchemaMeterDecimal>();
-        default:
-            break;
-    }
-
-    return nullptr;
+    return schemas->descriptions();
 }
 
-void UnitsApi::setSchema(UnitSystem system)
+std::vector<std::string> UnitsApi::getNames()
 {
-    if (UserPrefSystem) {
-        UserPrefSystem->resetSchemaUnits();  // for schemas changed the Quantity constants
-    }
-
-    UserPrefSystem = createSchema(system);
-    currentSystem = system;
-
-    // for wrong value fall back to standard schema
-    if (!UserPrefSystem) {
-        UserPrefSystem = std::make_unique<UnitsSchemaInternal>();
-        currentSystem = UnitSystem::SI1;
-    }
-
-    UserPrefSystem->setSchemaUnits();  // if necessary a unit schema can change the constants in
-                                       // Quantity (e.g. mi=1.8km rather then 1.6km).
+    return schemas->names();
 }
 
-std::string UnitsApi::toString(const Base::Quantity& quantity, const QuantityFormat& format)
+std::size_t UnitsApi::count()
+{
+    return static_cast<int>(schemas->count());
+}
+
+bool UnitsApi::isMultiUnitAngle()
+{
+    return schemas->currentSchema()->isMultiUnitAngle();
+}
+
+bool UnitsApi::isMultiUnitLength()
+{
+    return schemas->currentSchema()->isMultiUnitLength();
+}
+
+std::string UnitsApi::getBasicLengthUnit()
+{
+    return schemas->currentSchema()->getBasicLengthUnit();
+}
+
+std::size_t UnitsApi::getFractDenominator()
+{
+    return schemas->defFractDenominator();
+}
+
+std::unique_ptr<UnitsSchema> UnitsApi::createSchema(const std::size_t num)
+{
+    return std::make_unique<UnitsSchema>(schemas->spec(num));
+}
+
+void UnitsApi::setSchema(const std::string& name)
+{
+    schemas->select(name);
+}
+
+void UnitsApi::setSchema(const size_t num)
+{
+    schemas->select(num);
+}
+
+std::string UnitsApi::toString(const Quantity& quantity, const QuantityFormat& format)
 {
     return fmt::format("'{} {}'", toNumber(quantity, format), quantity.getUnit().getString());
 }
 
-std::string UnitsApi::toNumber(const Base::Quantity& quantity, const QuantityFormat& format)
+std::string UnitsApi::toNumber(const Quantity& quantity, const QuantityFormat& format)
 {
     return toNumber(quantity.getValue(), format);
 }
 
-std::string UnitsApi::toNumber(double value, const QuantityFormat& format)
+std::string UnitsApi::toNumber(const double value, const QuantityFormat& format)
 {
     std::stringstream ss;
 
@@ -154,31 +121,6 @@ std::string UnitsApi::toNumber(double value, const QuantityFormat& format)
     ss << std::setprecision(format.precision) << value;
 
     return ss.str();
-}
-
-// return true if the current user schema uses multiple units for length (ex. Ft/In)
-bool UnitsApi::isMultiUnitLength()
-{
-    return UserPrefSystem->isMultiUnitLength();
-}
-
-// return true if the current user schema uses multiple units for angles (ex. DMS)
-bool UnitsApi::isMultiUnitAngle()
-{
-    return UserPrefSystem->isMultiUnitAngle();
-}
-
-std::string UnitsApi::getBasicLengthUnit()
-{
-    return UserPrefSystem->getBasicLengthUnit();
-}
-
-// === static translation methods ==========================================
-
-std::string
-UnitsApi::schemaTranslate(const Base::Quantity& quant, double& factor, std::string& unitString)
-{
-    return UserPrefSystem->schemaTranslate(quant, factor, unitString);
 }
 
 double UnitsApi::toDouble(PyObject* args, const Base::Unit& u)
@@ -203,34 +145,30 @@ double UnitsApi::toDouble(PyObject* args, const Base::Unit& u)
     throw Base::UnitsMismatchError("Wrong parameter type!");
 }
 
-Quantity UnitsApi::toQuantity(PyObject* args, const Base::Unit& u)
+std::string
+UnitsApi::schemaTranslate(const Quantity& quant, double& factor, std::string& unitString)
 {
-    double d {};
-    if (PyUnicode_Check(args)) {
-        std::string str(PyUnicode_AsUTF8(args));
-        // Parse the string
-        Quantity q = Quantity::parse(str);
-        d = q.getValue();
-    }
-    else if (PyFloat_Check(args)) {
-        d = PyFloat_AsDouble(args);
-    }
-    else if (PyLong_Check(args)) {
-        d = static_cast<double>(PyLong_AsLong(args));
-    }
-    else {
-        throw Base::UnitsMismatchError("Wrong parameter type!");
-    }
-
-    return Quantity(d, u);
+    return schemas->currentSchema()->translate(quant, factor, unitString);
 }
 
-void UnitsApi::setDecimals(int prec)
+std::string UnitsApi::schemaTranslate(const Quantity& quant)
 {
-    UserPrefDecimals = prec;
+    double dummy1 {};  // to satisfy GCC
+    std::string dummy2;
+    return schemas->currentSchema()->translate(quant, dummy1, dummy2);
 }
 
-int UnitsApi::getDecimals()
+void UnitsApi::setDecimals(const std::size_t prec)
 {
-    return UserPrefDecimals;
+    decimals = prec;
+}
+
+size_t UnitsApi::getDecimals()
+{
+    return decimals;
+}
+
+size_t UnitsApi::getDefDecimals()
+{
+    return schemas->getDecimals();
 }

src/Base/UnitsApi.h
@@ -20,13 +20,12 @@
  *                                                                         *
  ***************************************************************************/
 
-
 #ifndef BASE_UNITSAPI_H
 #define BASE_UNITSAPI_H
 
-#include <memory>
-#include <QCoreApplication>
 #include "UnitsSchema.h"
+#include "UnitsSchemas.h"
+#include "UnitsSchemasData.h"
 #include "Quantity.h"
 
 
@@ -37,96 +36,71 @@ using PyMethodDef = struct PyMethodDef;
 
 namespace Base
 {
-using UnitsSchemaPtr = std::unique_ptr<UnitsSchema>;
 
-/**
- * The UnitsApi
- */
+
 class BaseExport UnitsApi
 {
-    Q_DECLARE_TR_FUNCTIONS(UnitsApi)
-
 public:
-    /** set Schema
-     * set the UnitsSchema of the Application
-     * this a represented by a class of type UnitSchema which
-     * defines a set of standard units for that schema and rules
-     * for representative strings.
-     */
-    static void setSchema(UnitSystem s);
-    /// return the active schema
-    static UnitSystem getSchema()
-    {
-        return currentSystem;
-    }
-    /// Returns a brief description of a schema
-    static QString getDescription(UnitSystem);
+    static void init();
+    static std::unique_ptr<UnitsSchema> createSchema(std::size_t num);
+    static void setSchema(const std::string& name);
+    static void setSchema(std::size_t num);
 
     static std::string
-    schemaTranslate(const Base::Quantity& quant, double& factor, std::string& unitString);
-    static std::string schemaTranslate(const Base::Quantity& quant)
-    {  // to satisfy GCC
-        double dummy1 {};
-        std::string dummy2;
-        return UnitsApi::schemaTranslate(quant, dummy1, dummy2);
-    }
+    schemaTranslate(const Quantity& quant, double& factor, std::string& unitString);
 
-    /** Get a number as string for a quantity of a given format.
-     * The string is a number in C locale (i.e. the decimal separator is always a dot) and if
-     * needed represented in scientific notation. The string also includes the unit of the quantity.
-     */
-    static std::string toString(const Base::Quantity& q,
-                                const QuantityFormat& f = QuantityFormat(QuantityFormat::Default));
-    /** Get a number as string for a quantity of a given format.
-     * The string is a number in C locale (i.e. the decimal separator is always a dot) and if
-     * needed represented in scientific notation. The string doesn't include the unit of the
-     * quantity.
-     */
-    static std::string toNumber(const Base::Quantity& q,
-                                const QuantityFormat& f = QuantityFormat(QuantityFormat::Default));
-    /** Get a number as string for a double of a given format.
-     * The string is a number in C locale (i.e. the decimal separator is always a dot) and if
-     * needed represented in scientific notation. The string doesn't include the unit of the
-     * quantity.
-     */
-    static std::string toNumber(double value,
-                                const QuantityFormat& f = QuantityFormat(QuantityFormat::Default));
+    static std::string schemaTranslate(const Quantity& quant);
 
-    /// generate a value for a quantity with default user preferred system
+    /**
+     * toString & toNumber:
+     * Quantity to string. Optionally apply format
+     * The string is a number in C locale (i.e. the decimal separator is always a dot)
+     * Scientific notation (if needed).
+     */
+
+    /** INCLUDES unit */
+    static std::string
+    toString(const Quantity& quantity,
+             const QuantityFormat& format = QuantityFormat(QuantityFormat::Default));
+
+    /** Does NOT include unit */
+    static std::string
+    toNumber(const Quantity& quantity,
+             const QuantityFormat& format = QuantityFormat(QuantityFormat::Default));
+
+    /** Does NOT include unit */
+    static std::string
+    toNumber(double value, const QuantityFormat& format = QuantityFormat(QuantityFormat::Default));
+
     static double toDouble(PyObject* args, const Base::Unit& u = Base::Unit());
-    /// generate a value for a quantity with default user preferred system
-    static Quantity toQuantity(PyObject* args, const Base::Unit& u = Base::Unit());
 
-    // set the number of decimals
-    static void setDecimals(int);
-    // get the number of decimals
-    static int getDecimals();
-    //@}
+    static void setDecimals(std::size_t);
+    static std::size_t getDecimals();
+    static std::size_t getDefDecimals();
 
-    // double Result;
+    static std::vector<std::string> getDescriptions();
+    static std::vector<std::string> getNames();
 
-    // return true if the current user schema uses multiple units for length (ex. Ft/In)
-    static bool isMultiUnitLength();
+    static std::size_t count();
 
-    // return true if the current user schema uses multiple units for angles (ex. DMS)
     static bool isMultiUnitAngle();
-
-    // return the basic unit of measure for length in the current user schema.
+    static bool isMultiUnitLength();
     static std::string getBasicLengthUnit();
+    static std::size_t getFractDenominator();
 
+    static std::size_t getDefSchemaNum()
+    {
+        return schemas->spec().num;
+    }
     // Python interface
     static PyMethodDef Methods[];
 
-    /// return an instance of the given enum value
-    static UnitsSchemaPtr createSchema(UnitSystem s);
-
 protected:
-    static UnitsSchemaPtr UserPrefSystem;
-    static UnitSystem currentSystem;
-    /// number of decimals for floats
-    static int UserPrefDecimals;
+    static inline auto schemas =
+        std::make_unique<UnitsSchemas>(UnitsSchemasData::unitSchemasDataPack);
+    static inline std::size_t decimals {2};
+    static inline std::size_t denominator {2};
 
-protected:
     // the python API wrapper methods
     static PyObject* sParseQuantity(PyObject* self, PyObject* args);
     static PyObject* sListSchemas(PyObject* self, PyObject* args);
@@ -137,6 +111,5 @@ protected:
 };
 
 }  // namespace Base
-
 
 #endif  // BASE_UNITSAPI_H

commit c96df6922b88f9cd16ea3fa686bcf08b0b954840

Base: fix Python interface for Unit schema selection
Fixes: baec0f8cee94 ("Base: simplify UnitsSchemas management")

src/Base/UnitsApiPy.cpp
@@ -135,7 +135,7 @@ PyObject* UnitsApi::sGetSchema(PyObject* /*self*/, PyObject* args)
         return nullptr;
     }
 
-    return Py_BuildValue("i", count());
+    return Py_BuildValue("i", schemas->currentSchema()->getNum());
 }
 
 PyObject* UnitsApi::sSetSchema(PyObject* /*self*/, PyObject* args)
@@ -164,7 +164,7 @@ PyObject* UnitsApi::sSchemaTranslate(PyObject* /*self*/, PyObject* args)
 
     if (index < 0 || index >= static_cast<int>(count())) {
         PyErr_SetString(PyExc_ValueError,
-                        std::string {"invalid schema index:" + std::to_string(index)}.c_str());
+                        std::string {"invalid schema index: " + std::to_string(index)}.c_str());
         return nullptr;
     }
 
@@ -172,7 +172,8 @@ PyObject* UnitsApi::sSchemaTranslate(PyObject* /*self*/, PyObject* args)
 
     double factor {};
     std::string unitStr;
-    const std::string unitStrLocalised = schemaTranslate(quant, factor, unitStr);
+    auto schema = std::make_unique<UnitsSchema>(schemas->spec(index));
+    const std::string unitStrLocalised = schema->translate(quant, factor, unitStr);
 
     Py::Tuple res {3};
     res[0] = Py::String {unitStrLocalised, "utf-8"};