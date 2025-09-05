southseact-3d/fcd

commit baec0f8cee94bb897dfd18843d9f282b59657d78

Base: simplify UnitsSchemas management
Fixes: Maintaining schemas is difficult and error-prone

- Facilitate easy schemas add, remove, change, etc.
- Remove 14 files containing approx 2,190 lines of if/else code and data
- Place data in one file (UnitsSchemasData.h) using a normalized structure (including special functions)
- Isolate and simplify data operations (code)
- Remove schemas enum to keep data independent of code
- Separate responsibilities: Specifications, data, schemas, schema
- Add schema data 'isDefault'
- Add schema data name
- Prefer algorithms to raw loops
- Add schemas unit tests
- Tweak quantity unit tests

src/Base/UnitsSchema.cpp
@@ -1,41 +1,97 @@
-/***************************************************************************
- *   Copyright (c) 2009 Jürgen Riegel <FreeCAD@juergen-riegel.net>         *
- *                                                                         *
- *   This file is part of the FreeCAD CAx development system.              *
- *                                                                         *
- *   This library is free software; you can redistribute it and/or         *
- *   modify it under the terms of the GNU Library General Public           *
- *   License as published by the Free Software Foundation; either          *
- *   version 2 of the License, or (at your option) any later version.      *
- *                                                                         *
- *   This library  is distributed in the hope that it will be useful,      *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU Library General Public License for more details.                  *
- *                                                                         *
- *   You should have received a copy of the GNU Library General Public     *
- *   License along with this library; see the file COPYING.LIB. If not,    *
- *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
- *   Suite 330, Boston, MA  02111-1307, USA                                *
- *                                                                         *
- ***************************************************************************/
+/************************************************************************
+ *                                                                      *
+ *   This file is part of the FreeCAD CAx development system.           *
+ *                                                                      *
+ *   This library is free software; you can redistribute it and/or      *
+ *   modify it under the terms of the GNU Library General Public        *
+ *   License as published by the Free Software Foundation; either       *
+ *   version 2 of the License, or (at your option) any later version.   *
+ *                                                                      *
+ *   This library  is distributed in the hope that it will be useful,   *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of     *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      *
+ *   GNU Library General Public License for more details.               *
+ *                                                                      *
+ *   You should have received a copy of the GNU Library General Public  *
+ *   License along with this library; see the file COPYING.LIB. If not, *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,      *
+ *   Suite 330, Boston, MA  02111-1307, USA                             *
+ *                                                                      *
+ ************************************************************************/
 
 #include "PreCompiled.h"
-#ifdef __GNUC__
-#include <unistd.h>
-#endif
+
+#include <iomanip>
+#include <sstream>
+#include <string>
 
 #include <QLocale>
 #include <QString>
 
 #include "Quantity.h"
 #include "UnitsSchema.h"
+#include "UnitsSchemasData.h"
+#include "UnitsSchemasSpecs.h"
+#include "Exception.h"
+#include "Quantity.h"
 
-using namespace Base;
+using Base::UnitsSchema;
+using Base::UnitsSchemaSpec;
 
-std::string UnitsSchema::toLocale(const Base::Quantity& quant,
-                                  double factor,
-                                  const std::string& unitString) const
+
+UnitsSchema::UnitsSchema(UnitsSchemaSpec spec)
+    : spec {std::move(spec)}
+{}
+
+std::string UnitsSchema::translate(const Quantity& quant) const
+{  // to satisfy GCC
+    double dummy1 {};
+    std::string dummy2;
+    return translate(quant, dummy1, dummy2);
+}
+
+std::string
+UnitsSchema::translate(const Quantity& quant, double& factor, std::string& unitString) const
+{
+    if (spec.translationSpecs.empty()) {
+        return toLocale(quant, 1.0, unitString);
+    }
+
+    const auto unitName = quant.getUnit().getTypeString();
+
+    if (spec.translationSpecs.count(unitName) == 0) {
+        // no schema-level translation. Use defaults.
+        factor = 1.0;
+        unitString = quant.getUnit().getString();
+
+        return toLocale(quant, factor, unitString);
+    }
+
+    const auto value = quant.getValue();
+
+    auto isSuitable = [&](const UnitTranslationSpec& row) {
+        return row.threshold > value || row.threshold == 0;  // zero indicates default
+    };
+
+    auto unitSpecs = spec.translationSpecs.at(unitName);
+    const auto unitSpec = std::find_if(unitSpecs.begin(), unitSpecs.end(), isSuitable);
+    if (unitSpec == unitSpecs.end()) {
+        throw RuntimeError("Suitable threshhold not found. Schema: " + spec.name
+                           + " value: " + std::to_string(value));
+    }
+
+    if (unitSpec->factor == 0) {
+        return UnitsSchemasData::runSpecial(unitSpec->unitString, value);
+    }
+
+    factor = unitSpec->factor;
+    unitString = unitSpec->unitString;
+
+    return toLocale(quant, factor, unitString);
+}
+
+std::string
+UnitsSchema::toLocale(const Quantity& quant, const double factor, const std::string& unitString)
 {
     QLocale Lc;
     const QuantityFormat& format = quant.getFormat();
@@ -44,6 +100,43 @@ std::string UnitsSchema::toLocale(const Base::Quantity& quant,
         Lc.setNumberOptions(static_cast<QLocale::NumberOptions>(opt));
     }
 
-    QString Ln = Lc.toString((quant.getValue() / factor), format.toFormat(), format.precision);
-    return QStringLiteral("%1 %2").arg(Ln, QString::fromStdString(unitString)).toStdString();
+    std::string valueString =
+        Lc.toString((quant.getValue() / factor), format.toFormat(), format.precision).toStdString();
+
+    return fmt::format(
+        "{}{}{}",
+        valueString,
+        unitString.empty() || unitString == "°" || unitString == "″" || unitString == "′" ? ""
+                                                                                          : " ",
+        unitString);
+}
+
+bool UnitsSchema::isMultiUnitLength() const
+{
+    return spec.isMultUnitLen;
+}
+
+bool UnitsSchema::isMultiUnitAngle() const
+{
+    return spec.isMultUnitAngle;
+}
+
+std::string UnitsSchema::getBasicLengthUnit() const
+{
+    return spec.basicLengthUnitStr;
+}
+
+std::string UnitsSchema::getName() const
+{
+    return spec.name;
+}
+
+std::string UnitsSchema::getDescription() const
+{
+    return spec.description;
+}
+
+int UnitsSchema::getNum() const
+{
+    return static_cast<int>(spec.num);
 }

src/App/Application.cpp
@@ -2795,12 +2795,10 @@ void Application::initApplication()
     // set up Unit system default
     const ParameterGrp::handle hGrp = GetApplication().GetParameterGroupByPath
        ("User parameter:BaseApp/Preferences/Units");
-    Base::UnitsApi::setSchema(static_cast<Base::UnitSystem>(hGrp->GetInt("UserSchema", 0)));
-    Base::UnitsApi::setDecimals(static_cast<int>(hGrp->GetInt("Decimals", Base::UnitsApi::getDecimals())));
-
-    // In case we are using fractional inches, get user setting for min unit
-    const int denom = static_cast<int>(hGrp->GetInt("FracInch", Base::QuantityFormat::getDefaultDenominator()));
-    Base::QuantityFormat::setDefaultDenominator(denom);
+    Base::UnitsApi::setSchema(hGrp->GetInt("UserSchema", Base::UnitsApi::getDefSchemaNum()));
+    Base::UnitsApi::setDecimals(hGrp->GetInt("Decimals", Base::UnitsApi::getDecimals()));
+    Base::QuantityFormat::setDefaultDenominator(
+        hGrp->GetInt("FracInch", Base::QuantityFormat::getDefaultDenominator()));
 
 #if defined (_DEBUG)
     Base::Console().Log("Application is built with debug information\n");

src/App/Document.cpp
@@ -876,13 +876,8 @@ Document::Document(const char* documentName)
                       "Additional tag to save the name of the company");
     ADD_PROPERTY_TYPE(UnitSystem, (""), 0, Prop_None, "Unit system to use in this project");
     // Set up the possible enum values for the unit system
-    int num = static_cast<int>(Base::UnitSystem::NumUnitSystemTypes);
-    std::vector<std::string> enumValsAsVector;
-    for (int i = 0; i < num; i++) {
-        QString item = Base::UnitsApi::getDescription(static_cast<Base::UnitSystem>(i));
-        enumValsAsVector.emplace_back(item.toStdString());
-    }
-    UnitSystem.setEnums(enumValsAsVector);
+
+    UnitSystem.setEnums(Base::UnitsApi::getDescriptions());
     // Get the preferences/General unit system as the default for a new document
     ParameterGrp::handle hGrpu =
         App::GetApplication().GetParameterGroupByPath("User parameter:BaseApp/Preferences/Units");

src/Base/CMakeLists.txt
@@ -150,20 +150,10 @@ SET(FreeCADBase_UNITAPI_SRCS
     UnitsApi.h
     UnitsSchema.h
     UnitsSchema.cpp
-    UnitsSchemaInternal.h
-    UnitsSchemaInternal.cpp
-    UnitsSchemaMKS.h
-    UnitsSchemaMKS.cpp
-    UnitsSchemaImperial1.h
-    UnitsSchemaImperial1.cpp
-    UnitsSchemaCentimeters.h
-    UnitsSchemaCentimeters.cpp
-    UnitsSchemaMmMin.h
-    UnitsSchemaMmMin.cpp
-    UnitsSchemaFemMilliMeterNewton.h
-    UnitsSchemaFemMilliMeterNewton.cpp
-    UnitsSchemaMeterDecimal.h
-    UnitsSchemaMeterDecimal.cpp
+    UnitsSchemas.cpp
+    UnitsSchemas.h
+    UnitsSchemasData.h
+    UnitsSchemasSpecs.h
     Quantity.h
     Quantity.cpp
     QuantityPyImp.cpp

src/Base/Quantity.cpp
@@ -64,7 +64,7 @@ int QuantityFormat::defaultDenominator = 8;  // for 1/8"
 QuantityFormat::QuantityFormat()
     : option(OmitGroupSeparator | RejectGroupSeparator)
     , format(Fixed)
-    , precision(UnitsApi::getDecimals())
+    , precision(static_cast<int>(UnitsApi::getDecimals()))
     , denominator(defaultDenominator)
 {}
 
@@ -256,14 +256,15 @@ std::string Quantity::getUserString(double& factor, std::string& unitString) con
 std::string
 Quantity::getUserString(UnitsSchema* schema, double& factor, std::string& unitString) const
 {
-    return schema->schemaTranslate(*this, factor, unitString);
+    return schema->translate(*this, factor, unitString);
 }
 
 std::string Quantity::getSafeUserString() const
 {
     auto userStr = getUserString();
     if (myValue != 0.0 && parse(userStr).getValue() == 0) {
-        userStr = fmt::format("{} {}", myValue, getUnit().getString());
+        auto unitStr = getUnit().getString();
+        userStr = fmt::format("{}{}{}", myValue, unitStr.empty() ? "" : " ", unitStr);
     }
 
     return Tools::escapeQuotesFromString(userStr);

src/Base/UnitsApi.cpp
@@ -20,124 +20,91 @@
  *                                                                         *
  ***************************************************************************/
 
-
 #include "PreCompiled.h"
-
-#include <sstream>
+#ifndef _PreComp_
 #include <iomanip>
-#include <memory>
+#include <sstream>
+#endif
 
 #include <CXX/WrapPython.h>
 #include <fmt/format.h>
-#include <QString>
+
 #include "Exception.h"
-
 #include "UnitsApi.h"
-#include "UnitsSchemaCentimeters.h"
-#include "UnitsSchemaInternal.h"
-#include "UnitsSchemaImperial1.h"
-#include "UnitsSchemaMKS.h"
-#include "UnitsSchemaMmMin.h"
-#include "UnitsSchemaFemMilliMeterNewton.h"
-#include "UnitsSchemaMeterDecimal.h"
+#include "UnitsSchema.h"
+#include "UnitsSchemas.h"
+#include "UnitsSchemasData.h"
 
-using namespace Base;
+using Base::UnitsApi;
+using Base::UnitsSchema;
+using Base::UnitsSchemas;
 
-// === static attributes  ================================================
-
-UnitsSchemaPtr UnitsApi::UserPrefSystem(new UnitsSchemaInternal());
-UnitSystem UnitsApi::currentSystem = UnitSystem::SI1;
-
-int UnitsApi::UserPrefDecimals = 2;
-
-QString UnitsApi::getDescription(UnitSystem system)
+void UnitsApi::init()
 {
-    switch (system) {
-        case UnitSystem::SI1:
-            return tr("Standard (mm, kg, s, °)");
-        case UnitSystem::SI2:
-            return tr("MKS (m, kg, s, °)");
-        case UnitSystem::Imperial1:
-            return tr("US customary (in, lb)");
-        case UnitSystem::ImperialDecimal:
-            return tr("Imperial decimal (in, lb)");
-        case UnitSystem::Centimeters:
-            return tr("Building Euro (cm, m², m³)");
-        case UnitSystem::ImperialBuilding:
-            return tr("Building US (ft-in, sqft, cft)");
-        case UnitSystem::MmMin:
-            return tr("Metric small parts & CNC (mm, mm/min)");
-        case UnitSystem::ImperialCivil:
-            return tr("Imperial for Civil Eng (ft, ft/s)");
-        case UnitSystem::FemMilliMeterNewton:
-            return tr("FEM (mm, N, s)");
-        case UnitSystem::MeterDecimal:
-            return tr("Meter decimal (m, m², m³)");
-        default:
-            return tr("Unknown schema");
-    }
+    schemas = std::make_unique<UnitsSchemas>(UnitsSchemasData::unitSchemasDataPack);
 }
 
-UnitsSchemaPtr UnitsApi::createSchema(UnitSystem system)
+std::vector<std::string> UnitsApi::getDescriptions()
 {
-    switch (system) {
-        case UnitSystem::SI1:
-            return std::make_unique<UnitsSchemaInternal>();
-        case UnitSystem::SI2:
-            return std::make_unique<UnitsSchemaMKS>();
-        case UnitSystem::Imperial1:
-            return std::make_unique<UnitsSchemaImperial1>();
-        case UnitSystem::ImperialDecimal:
-            return std::make_unique<UnitsSchemaImperialDecimal>();
-        case UnitSystem::Centimeters:
-            return std::make_unique<UnitsSchemaCentimeters>();
-        case UnitSystem::ImperialBuilding:
-            return std::make_unique<UnitsSchemaImperialBuilding>();
-        case UnitSystem::MmMin:
-            return std::make_unique<UnitsSchemaMmMin>();
-        case UnitSystem::ImperialCivil:
-            return std::make_unique<UnitsSchemaImperialCivil>();
-        case UnitSystem::FemMilliMeterNewton:
-            return std::make_unique<UnitsSchemaFemMilliMeterNewton>();
-        case UnitSystem::MeterDecimal:
-            return std::make_unique<UnitsSchemaMeterDecimal>();
-        default:
-            break;
-    }
-
-    return nullptr;
+    return schemas->descriptions();
 }
 
-void UnitsApi::setSchema(UnitSystem system)
+std::vector<std::string> UnitsApi::getNames()
 {
-    if (UserPrefSystem) {
-        UserPrefSystem->resetSchemaUnits();  // for schemas changed the Quantity constants
-    }
-
-    UserPrefSystem = createSchema(system);
-    currentSystem = system;
-
-    // for wrong value fall back to standard schema
-    if (!UserPrefSystem) {
-        UserPrefSystem = std::make_unique<UnitsSchemaInternal>();
-        currentSystem = UnitSystem::SI1;
-    }
-
-    UserPrefSystem->setSchemaUnits();  // if necessary a unit schema can change the constants in
-                                       // Quantity (e.g. mi=1.8km rather then 1.6km).
+    return schemas->names();
 }
 
-std::string UnitsApi::toString(const Base::Quantity& quantity, const QuantityFormat& format)
+std::size_t UnitsApi::count()
+{
+    return static_cast<int>(schemas->count());
+}
+
+bool UnitsApi::isMultiUnitAngle()
+{
+    return schemas->currentSchema()->isMultiUnitAngle();
+}
+
+bool UnitsApi::isMultiUnitLength()
+{
+    return schemas->currentSchema()->isMultiUnitLength();
+}
+
+std::string UnitsApi::getBasicLengthUnit()
+{
+    return schemas->currentSchema()->getBasicLengthUnit();
+}
+
+std::size_t UnitsApi::getFractDenominator()
+{
+    return schemas->defFractDenominator();
+}
+
+std::unique_ptr<UnitsSchema> UnitsApi::createSchema(const std::size_t num)
+{
+    return std::make_unique<UnitsSchema>(schemas->spec(num));
+}
+
+void UnitsApi::setSchema(const std::string& name)
+{
+    schemas->select(name);
+}
+
+void UnitsApi::setSchema(const size_t num)
+{
+    schemas->select(num);
+}
+
+std::string UnitsApi::toString(const Quantity& quantity, const QuantityFormat& format)
 {
     return fmt::format("'{} {}'", toNumber(quantity, format), quantity.getUnit().getString());
 }
 
-std::string UnitsApi::toNumber(const Base::Quantity& quantity, const QuantityFormat& format)
+std::string UnitsApi::toNumber(const Quantity& quantity, const QuantityFormat& format)
 {
     return toNumber(quantity.getValue(), format);
 }
 
-std::string UnitsApi::toNumber(double value, const QuantityFormat& format)
+std::string UnitsApi::toNumber(const double value, const QuantityFormat& format)
 {
     std::stringstream ss;
 
@@ -154,31 +121,6 @@ std::string UnitsApi::toNumber(double value, const QuantityFormat& format)
     ss << std::setprecision(format.precision) << value;
 
     return ss.str();
-}
-
-// return true if the current user schema uses multiple units for length (ex. Ft/In)
-bool UnitsApi::isMultiUnitLength()
-{
-    return UserPrefSystem->isMultiUnitLength();
-}
-
-// return true if the current user schema uses multiple units for angles (ex. DMS)
-bool UnitsApi::isMultiUnitAngle()
-{
-    return UserPrefSystem->isMultiUnitAngle();
-}
-
-std::string UnitsApi::getBasicLengthUnit()
-{
-    return UserPrefSystem->getBasicLengthUnit();
-}
-
-// === static translation methods ==========================================
-
-std::string
-UnitsApi::schemaTranslate(const Base::Quantity& quant, double& factor, std::string& unitString)
-{
-    return UserPrefSystem->schemaTranslate(quant, factor, unitString);
 }
 
 double UnitsApi::toDouble(PyObject* args, const Base::Unit& u)
@@ -203,34 +145,30 @@ double UnitsApi::toDouble(PyObject* args, const Base::Unit& u)
     throw Base::UnitsMismatchError("Wrong parameter type!");
 }
 
-Quantity UnitsApi::toQuantity(PyObject* args, const Base::Unit& u)
+std::string
+UnitsApi::schemaTranslate(const Quantity& quant, double& factor, std::string& unitString)
 {
-    double d {};
-    if (PyUnicode_Check(args)) {
-        std::string str(PyUnicode_AsUTF8(args));
-        // Parse the string
-        Quantity q = Quantity::parse(str);
-        d = q.getValue();
-    }
-    else if (PyFloat_Check(args)) {
-        d = PyFloat_AsDouble(args);
-    }
-    else if (PyLong_Check(args)) {
-        d = static_cast<double>(PyLong_AsLong(args));
-    }
-    else {
-        throw Base::UnitsMismatchError("Wrong parameter type!");
-    }
-
-    return Quantity(d, u);
+    return schemas->currentSchema()->translate(quant, factor, unitString);
 }
 
-void UnitsApi::setDecimals(int prec)
+std::string UnitsApi::schemaTranslate(const Quantity& quant)
 {
-    UserPrefDecimals = prec;
+    double dummy1 {};  // to satisfy GCC
+    std::string dummy2;
+    return schemas->currentSchema()->translate(quant, dummy1, dummy2);
 }
 
-int UnitsApi::getDecimals()
+void UnitsApi::setDecimals(const std::size_t prec)
 {
-    return UserPrefDecimals;
+    decimals = prec;
+}
+
+size_t UnitsApi::getDecimals()
+{
+    return decimals;
+}
+
+size_t UnitsApi::getDefDecimals()
+{
+    return schemas->getDecimals();
 }

commit d705252566d61ddfc4514dda6877e009e0e5bc21

Base: pass format specifiers to UnitsSchemasData::runSpecial
Make toFractional() aware of desired fractional inch denominator.

Fixes: baec0f8cee94 ("Base: simplify UnitsSchemas management")

src/Base/UnitsSchema.cpp
@@ -79,7 +79,13 @@ UnitsSchema::translate(const Quantity& quant, double& factor, std::string& unitS
     }
 
     if (unitSpec->factor == 0) {
-        return UnitsSchemasData::runSpecial(unitSpec->unitString, value, factor, unitString);
+        const QuantityFormat& format = quant.getFormat();
+        return UnitsSchemasData::runSpecial(unitSpec->unitString,
+                                            value,
+                                            format.getPrecision(),
+                                            format.getDenominator(),
+                                            factor,
+                                            unitString);
     }
 
     factor = unitSpec->factor;

src/Base/UnitsSchemasData.h
@@ -654,27 +654,27 @@ inline std::size_t greatestCommonDenominator(const std::size_t a, const std::siz
 /**
  * double -> [feet'] [inches" [+ fraction]"], e.g.: 3' 4" + 3/8"
  */
-inline std::string toFractional(const double value)
+inline std::string toFractional(const double value, std::size_t denominator)
 {
     constexpr auto inchPerFoot {12};
     constexpr auto mmPerInch {25.4};
 
     auto numFractUnits =
-        static_cast<std::size_t>(std::round(std::abs(value) / mmPerInch * defDenominator));
+        static_cast<std::size_t>(std::round(std::abs(value) / mmPerInch * denominator));
     if (numFractUnits == 0) {
         return "0";
     }
 
     const auto feet =
-        static_cast<std::size_t>(std::floor(numFractUnits / (inchPerFoot * defDenominator)));
-    numFractUnits -= inchPerFoot * defDenominator * feet;
+        static_cast<std::size_t>(std::floor(numFractUnits / (inchPerFoot * denominator)));
+    numFractUnits -= inchPerFoot * denominator * feet;
 
-    const auto inches = static_cast<std::size_t>(std::floor(numFractUnits / defDenominator));
-    const std::size_t fractNumerator = numFractUnits - (defDenominator * inches);
+    const auto inches = static_cast<std::size_t>(std::floor(numFractUnits / denominator));
+    std::size_t numerator = numFractUnits - (denominator * inches);
 
-    const std::size_t common_denom = greatestCommonDenominator(fractNumerator, defDenominator);
-    const std::size_t numerator = fractNumerator / common_denom;
-    const std::size_t denominator = defDenominator / common_denom;
+    const std::size_t common_denom = greatestCommonDenominator(numerator, denominator);
+    numerator /= common_denom;
+    denominator /= common_denom;
 
     bool addSpace {false};
     std::string result;
@@ -737,33 +737,40 @@ inline std::string toDms(const double value)
  */
 
 // clang-format off
-inline const std::map<std::string, std::function<std::string(double, double&, std::string&)>> specials
+inline const std::map<std::string, std::function<std::string(double, std::size_t, std::size_t, double&, std::string&)>> specials
 {
     {
-        { "toDMS"        , [](const double val, double& factor, std::string& unitString) {
+        { "toDMS"        , [](const double val, [[maybe_unused]] const std::size_t precision, [[maybe_unused]] const std::size_t denominator,
+                              double& factor, std::string& unitString) {
             factor = 1.0;
             unitString = "deg";
             return toDms(val);
         }},
-        { "toFractional" , [](const double val, double& factor, std::string& unitString) {
+        { "toFractional" , [](const double val, [[maybe_unused]] const std::size_t precision, const std::size_t denominator,
+                              double& factor, std::string& unitString) {
             factor = 25.4;
             unitString = "in";
-            return toFractional(val);
+            return toFractional(val, denominator);
         }}
     }
 };  // clang-format on
 
-inline std::string
-runSpecial(const std::string& name, const double value, double& factor, std::string& unitString)
+inline std::string runSpecial(const std::string& name,
+                              const double value,
+                              const std::size_t precision,
+                              const std::size_t denominator,
+                              double& factor,
+                              std::string& unitString)
 {
-    return specials.contains(name) ? specials.at(name)(value, factor, unitString) : "";
+    return specials.contains(name)
+        ? specials.at(name)(value, precision, denominator, factor, unitString)
+        : "";
 }
 
 
 /**
  * Build data pack
  */
-
 inline const UnitsSchemasDataPack unitSchemasDataPack {schemaSpecs, defDecimals, defDenominator};
 
 