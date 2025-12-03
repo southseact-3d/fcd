southseact-3d/fcd

commit c177e19b9c5f16122eeb5f4537f8cc965092e3d6

CAM: fixes bug with op creation/cancelation (#25800)

src/Mod/CAM/Path/Op/Base.py
@@ -550,7 +550,10 @@ class ObjectOp(object):
             job = self.job
         else:
             job = PathUtils.addToJob(obj)
-
+        if not job:
+            raise ValueError(
+                "No job associated with the operation. Please ensure the operation is part of a job."
+            )
         obj.Active = True
 
         features = self.opFeatures(obj)

src/Mod/CAM/Path/Op/Gui/Base.py
@@ -33,7 +33,6 @@ import Path.Op.Base as PathOp
 import Path.Op.Gui.Selection as PathSelection
 import Path.Tool.Controller as PathToolController
 from Path.Tool.library.ui.dock import ToolBitLibraryDock
-import PathGui
 import PathScripts.PathUtils as PathUtils
 import importlib
 from PySide.QtCore import QT_TRANSLATE_NOOP
@@ -422,9 +421,10 @@ class TaskPanelPage(object):
             self.tcEditor.controller.hide()
 
     def resetToolController(self, job, tc):
-        if self.obj is not None:
-            self.obj.ToolController = tc
-            self.setupToolController()
+        if self.obj is None:
+            return
+        self.obj.ToolController = tc
+        self.setupToolController()
 
     def copyToolController(self):
         oldTc = self.tcEditor.obj
@@ -1454,6 +1454,9 @@ def Create(res):
     this function directly, but calls the Activated() function of the Command object
     that is created in each operations Gui implementation."""
     FreeCAD.ActiveDocument.openTransaction("Create %s" % res.name)
+    if res.job is None:
+        FreeCAD.ActiveDocument.abortTransaction()
+        raise ValueError("No job selected. Operation creation aborted.")
     try:
         obj = res.objFactory(res.name, obj=None, parentJob=res.job)
         if obj.Proxy:
@@ -1502,6 +1505,17 @@ class CommandPathOp:
         return False
 
     def Activated(self):
+        jobs = PathUtils.GetJobs()
+        if not jobs:
+            return
+        job = PathUtils.UserInput.chooseJob(jobs)
+        if job is None:
+            return  # Abort if no job selected or canceled
+        self.res.job = job
+        return Create(self.res)
+
+    def setJob(self, job):
+        self.res.job = job
         return Create(self.res)
 
 

src/Mod/CAM/PathScripts/PathUtilsGui.py
@@ -26,7 +26,6 @@ import FreeCAD
 import Path
 import Path.Main.Gui.JobCmd as PathJobCmd
 import Path.Tool.Controller as PathToolController
-import PathGui
 import PathScripts.PathUtils as PathUtils
 from PySide import QtGui
 

commit 1946ee5c01ff229b38786ada5028a09290f33c7e

CAM: Fix job assignment and model/stock initialization in ObjectOp
This PR fixes a bug introduced with PR #25800
It updates the ObjectOp class to correctly assign the job, model,
and stock properties from the parentJob object. Previously, only
PathUtils.addToJob was called, which did not set these attributes directly.

This change ensures that when creating a new operation, the job, model,
and stock fields are properly initialized from the parent job. This
fixes issues where operations did not inherit the correct job context,
leading to missing or incorrect references to the model and stock, and
potential errors in downstream processing.

src/Mod/CAM/Path/Op/Base.py
@@ -336,7 +336,10 @@ class ObjectOp(object):
 
         if not hasattr(obj, "DoNotSetDefaultValues") or not obj.DoNotSetDefaultValues:
             if parentJob:
-                self.job = PathUtils.addToJob(obj, jobname=parentJob.Name)
+                self.job = parentJob
+                self.model = parentJob.Model.Group if parentJob.Model else []
+                self.stock = parentJob.Stock if hasattr(parentJob, "Stock") else None
+                PathUtils.addToJob(obj, jobname=parentJob.Name)
             job = self.setDefaultValues(obj)
             if job:
                 job.SetupSheet.Proxy.setOperationProperties(obj, name)