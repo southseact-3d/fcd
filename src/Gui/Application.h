FreeCAD/FreeCAD

Gui: Respect both content size and minimum width for buttons
This is a hacky fix for https://github.com/FreeCAD/FreeCAD/issues/23607

Basically after widget is shown or polished we enforce it's minimum size to at
least cover the minimum size hint - something that QSS ignores if min-width is
specified.

src/Gui/Application.h
@@ -236,6 +236,9 @@ public:
     void setStyleSheet(const QString& qssFile, bool tiledBackground);
     void reloadStyleSheet();
     QString replaceVariablesInQss(const QString& qssText);
+
+    /// Set QStyle by name
+    void setStyle(const QString& style);
     //@}
 
     /** @name User Commands */

southseact-3d/fcd

(cherry picked from commit aefd2592a878fbf02bc9d97f0df9fc678b34951e)

src/Gui/Application.h
@@ -236,6 +236,9 @@ public:
     void setStyleSheet(const QString& qssFile, bool tiledBackground);
     void reloadStyleSheet();
     QString replaceVariablesInQss(const QString& qssText);
+
+    /// Set QStyle by name
+    void setStyle(const QString& name);
     //@}
 
     /** @name User Commands */