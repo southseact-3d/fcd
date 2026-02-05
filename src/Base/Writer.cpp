kadet1090/FreeCAD

Base: Add C++ iostate methods to Writer class

src/Base/Writer.cpp
@@ -93,6 +93,31 @@ Writer::Writer()
 
 Writer::~Writer() = default;
 
+void Writer::clear()
+{
+    Stream().clear();
+}
+
+bool Writer::good() const
+{
+    return Stream().good();
+}
+
+bool Writer::fail() const
+{
+    return Stream().fail();
+}
+
+bool Writer::bad() const
+{
+    return Stream().bad();
+}
+
+bool Writer::eof() const
+{
+    return Stream().eof();
+}
+
 std::ostream& Writer::beginCharStream(CharStreamFormat format)
 {
     if (CharStream) {

southseact-3d/fcd

src/Base/Writer.cpp
@@ -92,6 +92,31 @@ Writer::Writer()
 
 Writer::~Writer() = default;
 
+void Writer::clear()
+{
+    Stream().clear();
+}
+
+bool Writer::isGood() const
+{
+    return Stream().good();
+}
+
+bool Writer::hasFailed() const
+{
+    return Stream().fail();
+}
+
+bool Writer::isBad() const
+{
+    return Stream().bad();
+}
+
+bool Writer::isEof() const
+{
+    return Stream().eof();
+}
+
 std::ostream& Writer::beginCharStream(CharStreamFormat format)
 {
     if (CharStream) {